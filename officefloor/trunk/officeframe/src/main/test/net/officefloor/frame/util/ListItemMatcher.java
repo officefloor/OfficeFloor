d42a670c1cc2821cb0d37e1e1b47ff829ad9f85f
sagenschneider <sagenschneider@ee502a36-3d87-488a-bc83-848c733a9ae9>
2007-09-24 21:42:17 +0000

    mvn clean test-compile  (successful for all modules)

--- officefloor/trunk/officeframe/src/main/test/net/officefloor/frame/util/ListItemMatcher.java
+++ officefloor/trunk/officeframe/src/main/test/net/officefloor/frame/util/ListItemMatcher.java
@@ -14,7 +14,7 @@
  *  if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
  *  MA 02111-1307 USA
  */
-package net.officefloor.frame.util;
+package net.officefloor.frame.test;
 
 import junit.framework.AssertionFailedError;
 
