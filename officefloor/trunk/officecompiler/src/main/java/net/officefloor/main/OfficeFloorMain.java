0271ac0ea7f46e3d3b9769788131189d33a8c4a1
Daniel <sagenschneider@users.noreply.github.com>
2007-12-14 21:05:03 +0000

    Allowing TaskFactory to specify from configuration.

--- officefloor/trunk/officecompiler/src/main/java/net/officefloor/main/OfficeFloorMain.java
+++ officefloor/trunk/officecompiler/src/main/java/net/officefloor/main/OfficeFloorMain.java
@@ -22,7 +22,9 @@ import net.officefloor.LoaderContext;
 import net.officefloor.compile.OfficeFloorCompiler;
 import net.officefloor.frame.api.OfficeFrame;
 import net.officefloor.frame.api.build.BuilderFactory;
+import net.officefloor.frame.api.manage.Office;
 import net.officefloor.frame.api.manage.OfficeFloor;
+import net.officefloor.frame.api.manage.WorkManager;
 import net.officefloor.repository.ConfigurationContext;
 import net.officefloor.repository.ConfigurationItem;
 import net.officefloor.repository.classloader.ClassLoaderConfigurationContext;
@@ -57,7 +59,7 @@ public class OfficeFloorMain {
 		// Ensure have command line arguments
 		if (args.length == 0) {
 			System.err.println("USAGE: java ... " + OFFICE_FLOOR_MAIN
-					+ " <office floor configuration file>");
+					+ " <office floor configuration file> [<office> <work>]");
 			return;
 		}
 
@@ -65,10 +67,40 @@ public class OfficeFloorMain {
 		String officeFloorConfigFile = args[0];
 
 		// Create the office floor
+		System.out.println("Creating office floor '" + officeFloorConfigFile
+				+ "'");
 		OfficeFloor officeFloor = createOfficeFloor(officeFloorConfigFile);
 
 		// Open the office floor
+		System.out.println("Opening office floor");
 		officeFloor.openOfficeFloor();
+
+		// Determine if invoke work
+		if (args.length >= 3) {
+			// Obtain office and work for invoking
+			String officeName = args[1];
+			String workName = args[2];
+			System.out.println("Invoking work '" + workName + "' on office '"
+					+ officeName + "'");
+
+			// Obtain the Office
+			Office office = officeFloor.getOffice(officeName);
+			if (office == null) {
+				System.err.println("No office by name '" + officeName + "'");
+				return;
+			}
+
+			// Obtain the Work
+			WorkManager workManager = office.getWorkManager(workName);
+			if (workManager == null) {
+				System.err.println("No work by name '" + workName
+						+ "' on office " + officeName);
+			}
+
+			// Invoke the Work
+			String parameter = (args.length > 4 ? args[3] : null);
+			workManager.invokeWork(parameter);
+		}
 	}
 
 	/**
