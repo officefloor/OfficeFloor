ddba3ee4e919e9d44b46d49032b867ea295263b3
sagenschneider <sagenschneider@ee502a36-3d87-488a-bc83-848c733a9ae9>
2007-12-20 23:55:08 +0000

    Launching Office Floor.

--- officefloor/trunk/officeeclipse/src/main/java/net/officefloor/eclipse/launch/OfficeFloorLauncher.java
+++ officefloor/trunk/officeeclipse/src/main/java/net/officefloor/eclipse/launch/OfficeFloorLauncher.java
@@ -17,6 +17,7 @@
 package net.officefloor.eclipse.launch;
 
 import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchConfigurationType;
 
 import net.officefloor.frame.api.manage.OfficeFloor;
 import net.officefloor.frame.api.manage.WorkManager;
@@ -28,6 +29,12 @@ import net.officefloor.model.office.OfficeModel;
  * @author Daniel
  */
 public class OfficeFloorLauncher {
+
+	/**
+	 * {@link ILaunchConfigurationType} identifier for running the
+	 * {@link OfficeFloor}.
+	 */
+	public static final String ID_OFFICE_FLOOR_CONFIGURATION_TYPE = "net.officefloor.eclipse.launch.configurationtype.officeFloor";
 
 	/**
 	 * {@link ILaunchConfiguration} attribute to specify the {@link OfficeFloor}
