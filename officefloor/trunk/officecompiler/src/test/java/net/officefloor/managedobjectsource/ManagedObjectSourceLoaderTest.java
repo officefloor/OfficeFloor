5acb27f2d9c01020e8d7af6736946ba5ee3e3252
Daniel <sagenschneider@users.noreply.github.com>
2008-02-20 23:23:22 +0000

    Providing FlowNodesEnhancer to link in flow nodes added by a ManagedObjectSource.

---
+++ officefloor/trunk/officecompiler/src/test/java/net/officefloor/managedobjectsource/ManagedObjectSourceLoaderTest.java
@@ -0,0 +1,48 @@
+/*
+ *  Office Floor, Application Server
+ *  Copyright (C) 2006 Daniel Sagenschneider
+ *
+ *  This program is free software; you can redistribute it and/or modify it under the terms 
+ *  of the GNU General Public License as published by the Free Software Foundation; either 
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with this program; 
+ *  if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *  MA 02111-1307 USA
+ */
+package net.officefloor.managedobjectsource;
+
+import java.util.Properties;
+
+import junit.framework.TestCase;
+import net.officefloor.model.officefloor.ManagedObjectSourceModel;
+
+/**
+ * Tests the {@link ManagedObjectSourceLoader}.
+ * 
+ * @author Daniel
+ */
+public class ManagedObjectSourceLoaderTest extends TestCase {
+
+	/**
+	 * Ensures loads.
+	 */
+	public void testLoadManagedObjectSource() throws Throwable {
+
+		// Create the loader
+		ManagedObjectSourceLoader loader = new ManagedObjectSourceLoader();
+
+		// Load the managed object source model
+		ManagedObjectSourceModel model = loader.loadManagedObjectSource("test",
+				new TestManagedObjectSource(), new Properties(), this
+						.getClass().getClassLoader());
+
+		// Validate model
+		assertEquals("Incorrect managed object source class",
+				TestManagedObjectSource.class.getName(), model.getSource());
+	}
+}
---
+++ officefloor/trunk/officecompiler/src/test/java/net/officefloor/managedobjectsource/TestManagedObjectSource.java
@@ -0,0 +1,121 @@
+/*
+ *  Office Floor, Application Server
+ *  Copyright (C) 2006 Daniel Sagenschneider
+ *
+ *  This program is free software; you can redistribute it and/or modify it under the terms 
+ *  of the GNU General Public License as published by the Free Software Foundation; either 
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with this program; 
+ *  if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *  MA 02111-1307 USA
+ */
+package net.officefloor.managedobjectsource;
+
+import junit.framework.Assert;
+import net.officefloor.frame.api.build.HandlerBuilder;
+import net.officefloor.frame.api.build.Indexed;
+import net.officefloor.frame.api.build.ManagedObjectHandlerBuilder;
+import net.officefloor.frame.api.execute.Work;
+import net.officefloor.frame.internal.structure.FlowInstigationStrategyEnum;
+import net.officefloor.frame.spi.managedobject.ManagedObject;
+import net.officefloor.frame.spi.managedobject.source.ManagedObjectSource;
+import net.officefloor.frame.spi.managedobject.source.ManagedObjectSourceContext;
+import net.officefloor.frame.spi.managedobject.source.ManagedObjectTaskBuilder;
+import net.officefloor.frame.spi.managedobject.source.ManagedObjectWorkBuilder;
+import net.officefloor.frame.spi.managedobject.source.impl.AbstractManagedObjectSource;
+
+/**
+ * {@link ManagedObjectSource} to provide testing of
+ * {@link ManagedObjectSourceLoader}.
+ * 
+ * @author Daniel
+ */
+public class TestManagedObjectSource extends AbstractManagedObjectSource {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see net.officefloor.frame.spi.managedobject.source.impl.AbstractAsyncManagedObjectSource#loadSpecification(net.officefloor.frame.spi.managedobject.source.impl.AbstractAsyncManagedObjectSource.SpecificationContext)
+	 */
+	@Override
+	protected void loadSpecification(SpecificationContext context) {
+		context.addProperty("test");
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see net.officefloor.frame.spi.managedobject.source.impl.AbstractAsyncManagedObjectSource#loadMetaData(net.officefloor.frame.spi.managedobject.source.impl.AbstractAsyncManagedObjectSource.MetaDataContext)
+	 */
+	@Override
+	protected void loadMetaData(MetaDataContext context) throws Exception {
+
+		// Provide meta-data
+		context.setObjectClass(Object.class);
+
+		// Obtain the managed object source context for additional configuration
+		ManagedObjectSourceContext sourceContext = context
+				.getManagedObjectSourceContext();
+
+		// Register handler with flows
+		ManagedObjectHandlerBuilder<HandlerKey> managedObjectHandlerBuilder = sourceContext
+				.getHandlerBuilder(HandlerKey.class);
+		HandlerBuilder<HandlerFlowKey> handlerBuilder = managedObjectHandlerBuilder
+				.registerHandler(HandlerKey.HANDLER, HandlerFlowKey.class);
+		handlerBuilder.linkProcess(0, null, null);
+
+		// Register a task with the office
+		ManagedObjectWorkBuilder<Work> workBuilder = sourceContext.addWork(
+				"WORK", Work.class);
+		ManagedObjectTaskBuilder<Indexed> taskBuilder = workBuilder.addTask(
+				"TASK", Object.class, null);
+		taskBuilder.linkFlow(0, null, FlowInstigationStrategyEnum.SEQUENTIAL);
+		taskBuilder.linkFlow(1, null, FlowInstigationStrategyEnum.PARALLEL);
+
+		// Register recycle work
+		ManagedObjectWorkBuilder<Work> recycleWorkBuilder = sourceContext
+				.getRecycleWork(Work.class);
+		ManagedObjectTaskBuilder<Indexed> recycleOneTaskBuilder = recycleWorkBuilder
+				.addTask("RECYCLE TASK ONE", Object.class, null);
+		recycleOneTaskBuilder.linkFlow(0, "RECYCLE TASK TWO",
+				FlowInstigationStrategyEnum.PARALLEL);
+		recycleOneTaskBuilder.linkFlow(1, null,
+				FlowInstigationStrategyEnum.SEQUENTIAL);
+		ManagedObjectTaskBuilder<Indexed> recycleTwoTaskBuilder = recycleWorkBuilder
+				.addTask("RECYCLE TASK TWO", Object.class, null);
+		recycleTwoTaskBuilder.linkFlow(0, "RECYCLE TASK ONE",
+				FlowInstigationStrategyEnum.SEQUENTIAL);
+		recycleTwoTaskBuilder.setNextTaskInFlow("RECYCLE TASK ONE");
+		recycleTwoTaskBuilder.setTeam("Recycle Team");
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see net.officefloor.frame.spi.managedobject.source.impl.AbstractManagedObjectSource#getManagedObject()
+	 */
+	@Override
+	protected ManagedObject getManagedObject() throws Throwable {
+		Assert.fail("getManagedObject should not be invoked");
+		return null;
+	}
+
+	/**
+	 * Handler keys.
+	 */
+	public static enum HandlerKey {
+		HANDLER
+	}
+
+	/**
+	 * Handler flow keys.
+	 */
+	public static enum HandlerFlowKey {
+		FLOW_ONE, FLOW_TWO
+	}
+}
