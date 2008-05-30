1c2a57200ac5b3e4789f904ae33d246751d82933
Daniel <sagenschneider@users.noreply.github.com>
2008-05-30 21:24:08 +0000

    Handler link process to task of office.

---
+++ officefloor/trunk/officeeclipse/src/main/java/net/officefloor/eclipse/common/dialog/OfficeTaskDialog.java
@@ -0,0 +1,170 @@
+/*
+ *  Office Floor, Application Server
+ *  Copyright (C) 2006 Daniel Sagenschneider
+ *
+ *  This program is free software; you can redistribute it and/or modify it under the terms 
+ *  of the GNU General Public License as published by the Free Software Foundation; either 
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with this program; 
+ *  if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *  MA 02111-1307 USA
+ */
+package net.officefloor.eclipse.common.dialog;
+
+import net.officefloor.eclipse.classpath.ProjectClassLoader;
+import net.officefloor.model.office.OfficeModel;
+import net.officefloor.model.officefloor.OfficeFloorOfficeModel;
+import net.officefloor.model.officefloor.OfficeTaskModel;
+import net.officefloor.officefloor.OfficeFloorLoader;
+import net.officefloor.repository.ConfigurationContext;
+import net.officefloor.repository.ConfigurationItem;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.draw2d.ColorConstants;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.widgets.Shell;
+
+/**
+ * {@link Dialog} to create an {@link OfficeTaskModel}.
+ * 
+ * @author Daniel
+ */
+public class OfficeTaskDialog extends Dialog {
+
+	/**
+	 * Listing of {@link OfficeTaskModel} instances to choose from.
+	 */
+	private final OfficeTaskModel[] officeTasks;
+
+	/**
+	 * List of {@link OfficeTaskModel} instances.
+	 */
+	private List officeTaskList;
+
+	/**
+	 * Reports errors.
+	 */
+	private Label errorText;
+
+	/**
+	 * {@link OfficeTaskModel} to return.
+	 */
+	private OfficeTaskModel officeTask;
+
+	/**
+	 * Initiate.
+	 * 
+	 * @param parentShell
+	 *            Parent {@link Shell}.
+	 * @param office
+	 *            {@link OfficeFloorOfficeModel} to select an
+	 *            {@link OfficeTaskModel} from.
+	 * @param project
+	 *            {@link IProject} to obtain the {@link ConfigurationItem} of
+	 *            the {@link OfficeModel}.
+	 * @throws Exception
+	 *             If fails to initialise.
+	 */
+	public OfficeTaskDialog(Shell parentShell, OfficeFloorOfficeModel office,
+			IProject project) throws Exception {
+		super(parentShell);
+
+		// Obtain the configuration context of the project
+		ConfigurationContext configurationContext = ProjectClassLoader.create(
+				project).getConfigurationContext();
+
+		// Obtain the listing of office tasks
+		this.officeTasks = new OfficeFloorLoader().loadOfficeTasks(office,
+				configurationContext);
+	}
+
+	/**
+	 * Creates the {@link OfficeTaskModel}.
+	 * 
+	 * @return {@link OfficeTaskModel} or <code>null</code> if not created.
+	 */
+	public OfficeTaskModel createOfficeTask() {
+
+		// Block to open
+		this.setBlockOnOpen(true);
+		this.open();
+
+		// Return the office task
+		return this.officeTask;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.jface.dialogs.Dialog#createDialogArea(org.eclipse.swt.widgets.Composite)
+	 */
+	@Override
+	protected Control createDialogArea(Composite parent) {
+
+		// Create parent composite
+		Composite composite = (Composite) super.createDialogArea(parent);
+
+		// Selection of office tasks
+		new Label(composite, SWT.WRAP).setText("Select Office task:");
+		this.officeTaskList = new List(composite, SWT.SINGLE | SWT.BORDER);
+		for (OfficeTaskModel officeTask : this.officeTasks) {
+			this.officeTaskList.add(officeTask.getWorkName() + "."
+					+ officeTask.getTaskName());
+		}
+
+		// Error text
+		this.errorText = new Label(composite, SWT.WRAP);
+		this.errorText.setText("");
+		this.errorText.setLayoutData(new GridData(GridData.GRAB_HORIZONTAL
+				| GridData.HORIZONTAL_ALIGN_FILL));
+		this.errorText.setBackground(errorText.getDisplay().getSystemColor(
+				SWT.COLOR_WIDGET_BACKGROUND));
+		this.errorText.setForeground(ColorConstants.red);
+
+		// Return the composite
+		return composite;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.jface.dialogs.Dialog#okPressed()
+	 */
+	@Override
+	protected void okPressed() {
+		try {
+
+			// Ensure item selected
+			int[] selected = this.officeTaskList.getSelectionIndices();
+			if ((selected == null) || (selected.length != 1)) {
+				// Single item not selected
+				this.errorText.setText("Must select a task");
+				return;
+			}
+
+			// Obtain the office task
+			this.officeTask = this.officeTasks[selected[0]];
+
+		} catch (Throwable ex) {
+			// Failed, report error and do not close dialog
+			this.errorText.setText(ex.getClass().getSimpleName() + ": "
+					+ ex.getMessage());
+			return;
+		}
+
+		// Successful
+		super.okPressed();
+	}
+
+}
---
+++ officefloor/trunk/officeeclipse/src/main/java/net/officefloor/eclipse/officefloor/editparts/OfficeTaskEditPart.java
@@ -0,0 +1,93 @@
+/*
+ *  Office Floor, Application Server
+ *  Copyright (C) 2006 Daniel Sagenschneider
+ *
+ *  This program is free software; you can redistribute it and/or modify it under the terms 
+ *  of the GNU General Public License as published by the Free Software Foundation; either 
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with this program; 
+ *  if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *  MA 02111-1307 USA
+ */
+package net.officefloor.eclipse.officefloor.editparts;
+
+import java.beans.PropertyChangeEvent;
+import java.util.List;
+
+import net.officefloor.eclipse.common.editparts.AbstractOfficeFloorNodeEditPart;
+import net.officefloor.eclipse.common.editparts.PropertyChangeHandler;
+import net.officefloor.eclipse.officefloor.figure.OfficeTaskFigure;
+import net.officefloor.model.officefloor.OfficeTaskModel;
+import net.officefloor.model.officefloor.OfficeTaskModel.OfficeTaskEvent;
+
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.gef.EditPart;
+
+/**
+ * {@link EditPart} for the {@link OfficeTaskModel}.
+ * 
+ * @author Daniel
+ */
+public class OfficeTaskEditPart extends
+		AbstractOfficeFloorNodeEditPart<OfficeTaskModel> {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see net.officefloor.eclipse.common.editparts.AbstractOfficeFloorEditPart#populatePropertyChangeHandlers(java.util.List)
+	 */
+	@Override
+	protected void populatePropertyChangeHandlers(
+			List<PropertyChangeHandler<?>> handlers) {
+		handlers.add(new PropertyChangeHandler<OfficeTaskEvent>(OfficeTaskEvent
+				.values()) {
+			@Override
+			protected void handlePropertyChange(OfficeTaskEvent property,
+					PropertyChangeEvent evt) {
+				switch (property) {
+				case ADD_LINK_PROCESS:
+				case REMOVE_LINK_PROCESS:
+					OfficeTaskEditPart.this.refreshTargetConnections();
+					break;
+				}
+			}
+		});
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.gef.editparts.AbstractGraphicalEditPart#createFigure()
+	 */
+	@Override
+	protected IFigure createFigure() {
+		return new OfficeTaskFigure(this.getCastedModel().getWorkName() + "."
+				+ this.getCastedModel().getTaskName());
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see net.officefloor.eclipse.common.editparts.AbstractOfficeFloorNodeEditPart#populateConnectionSourceModels(java.util.List)
+	 */
+	@Override
+	protected void populateConnectionSourceModels(List<Object> models) {
+		// Never a source
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see net.officefloor.eclipse.common.editparts.AbstractOfficeFloorNodeEditPart#populateConnectionTargetModels(java.util.List)
+	 */
+	@Override
+	protected void populateConnectionTargetModels(List<Object> models) {
+		models.addAll(this.getCastedModel().getLinkProcesses());
+	}
+
+}
