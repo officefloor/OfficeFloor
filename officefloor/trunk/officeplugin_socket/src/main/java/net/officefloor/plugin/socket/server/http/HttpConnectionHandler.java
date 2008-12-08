5990e99fef0eab2f25acff6f58607ea2843c7531
sagenschneider <sagenschneider@ee502a36-3d87-488a-bc83-848c733a9ae9>
2008-12-08 18:05:55 +0000

    mvn clean install   working with all projects.

--- officefloor/trunk/officeplugin_socket/src/main/java/net/officefloor/plugin/socket/server/http/HttpConnectionHandler.java
+++ officefloor/trunk/officeplugin_socket/src/main/java/net/officefloor/plugin/socket/server/http/HttpConnectionHandler.java
@@ -19,7 +19,6 @@ package net.officefloor.plugin.socket.server.http;
 import java.io.IOException;
 
 import net.officefloor.plugin.socket.server.http.parse.HttpRequestParser;
-import net.officefloor.plugin.socket.server.http.parse.HttpRequestParserTest;
 import net.officefloor.plugin.socket.server.http.parse.ParseException;
 import net.officefloor.plugin.socket.server.spi.Connection;
 import net.officefloor.plugin.socket.server.spi.ConnectionHandler;
@@ -121,7 +120,7 @@ public class HttpConnectionHandler implements ConnectionHandler {
 	 */
 
 	/**
-	 * {@link HttpRequestParserTest}.
+	 * {@link HttpRequestParser}.
 	 */
 	private HttpRequestParser httpRequestParser;
 
