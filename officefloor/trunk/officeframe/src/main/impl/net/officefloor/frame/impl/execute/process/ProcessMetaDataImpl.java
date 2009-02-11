888346e725d948b29d0b6113cc352cd2ce3de395
sagenschneider <sagenschneider@ee502a36-3d87-488a-bc83-848c733a9ae9>
2009-02-11 00:32:53 +0000

    Refactoring work so far:   - using scopes Process, Thread, Work   - using OfficeFloorIssues to capture issues (rather than failing on first)
    (saving work so far as going on vacation)

---
+++ officefloor/trunk/officeframe/src/main/impl/net/officefloor/frame/impl/execute/process/ProcessMetaDataImpl.java
@@ -0,0 +1,77 @@
+/*
+ *  Office Floor, Application Server
+ *  Copyright (C) 2006 Daniel Sagenschneider
+ *
+ *  This program is free software; you can redistribute it and/or modify it under the terms 
+ *  of the GNU General Public License as published by the Free Software Foundation; either 
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with this program; 
+ *  if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *  MA 02111-1307 USA
+ */
+package net.officefloor.frame.impl.execute.process;
+
+import net.officefloor.frame.internal.structure.AdministratorMetaData;
+import net.officefloor.frame.internal.structure.ManagedObjectMetaData;
+import net.officefloor.frame.internal.structure.ProcessMetaData;
+
+/**
+ * {@link ProcessMetaData} implementation.
+ * 
+ * @author Daniel
+ */
+public class ProcessMetaDataImpl implements ProcessMetaData {
+
+	/**
+	 * {@link ManagedObjectMetaData} instances.
+	 */
+	private final ManagedObjectMetaData<?>[] managedObjectMetaData;
+
+	/**
+	 * {@link AdministratorMetaData} instances.
+	 */
+	private final AdministratorMetaData<?, ?>[] administratorMetaData;
+
+	/**
+	 * Initiate.
+	 * 
+	 * @param managedObjectMetaData
+	 *            {@link ManagedObjectMetaData} instances.
+	 * @param administratorMetaData
+	 *            {@link AdministratorMetaData} instances.
+	 */
+	public ProcessMetaDataImpl(
+			ManagedObjectMetaData<?>[] managedObjectMetaData,
+			AdministratorMetaData<?, ?>[] administratorMetaData) {
+		this.managedObjectMetaData = managedObjectMetaData;
+		this.administratorMetaData = administratorMetaData;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @seenet.officefloor.frame.internal.structure.ProcessMetaData#
+	 * getManagedObjectMetaData()
+	 */
+	@Override
+	public ManagedObjectMetaData<?>[] getManagedObjectMetaData() {
+		return this.managedObjectMetaData;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @seenet.officefloor.frame.internal.structure.ProcessMetaData#
+	 * getAdministratorMetaData()
+	 */
+	@Override
+	public AdministratorMetaData<?, ?>[] getAdministratorMetaData() {
+		return this.administratorMetaData;
+	}
+
+}
---
+++ officefloor/trunk/officeframe/src/main/impl/net/officefloor/frame/impl/execute/work/WorkManagerImpl.java
@@ -0,0 +1,112 @@
+/*
+ *  Office Floor, Application Server
+ *  Copyright (C) 2006 Daniel Sagenschneider
+ *
+ *  This program is free software; you can redistribute it and/or modify it under the terms 
+ *  of the GNU General Public License as published by the Free Software Foundation; either 
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with this program; 
+ *  if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *  MA 02111-1307 USA
+ */
+package net.officefloor.frame.impl.execute.work;
+
+import net.officefloor.frame.api.execute.FlowFuture;
+import net.officefloor.frame.api.execute.Work;
+import net.officefloor.frame.api.manage.NoInitialTaskException;
+import net.officefloor.frame.api.manage.WorkManager;
+import net.officefloor.frame.impl.execute.office.OfficeImpl;
+import net.officefloor.frame.internal.structure.FlowMetaData;
+import net.officefloor.frame.internal.structure.JobNode;
+import net.officefloor.frame.internal.structure.WorkMetaData;
+import net.officefloor.frame.spi.managedobject.ManagedObject;
+
+/**
+ * Implementation of the {@link WorkManager}.
+ * 
+ * @author Daniel
+ */
+public class WorkManagerImpl<W extends Work> implements WorkManager {
+
+	/**
+	 * Name of the {@link Work}.
+	 */
+	private final String workName;
+
+	/**
+	 * {@link WorkMetaData}.
+	 */
+	protected final WorkMetaData<W> workMetaData;
+
+	/**
+	 * {@link OfficeImpl} where this {@link Work} is being executed.
+	 */
+	protected final OfficeImpl office;
+
+	/**
+	 * Initiate.
+	 * 
+	 * @param workName
+	 *            Name of the {@link Work}.
+	 * @param workMetaData
+	 *            {@link WorkMetaData}.
+	 * @param office
+	 *            {@link OfficeImpl}.
+	 */
+	public WorkManagerImpl(String workName, WorkMetaData<W> workMetaData,
+			OfficeImpl office) {
+		this.workName = workName;
+		this.workMetaData = workMetaData;
+		this.office = office;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * net.officefloor.frame.api.manage.WorkManager#invokeWork(java.lang.Object)
+	 */
+	public FlowFuture invokeWork(Object parameter)
+			throws NoInitialTaskException {
+
+		// Obtain the Initial Flow meta-data
+		FlowMetaData<W> flowMetaData = this.workMetaData
+				.getInitialFlowMetaData();
+
+		// Ensure there is an initial task for the work
+		if (flowMetaData.getInitialTaskMetaData() == null) {
+			throw new NoInitialTaskException("No initial task for work '"
+					+ this.workName + "'");
+		}
+
+		// Create the job node within a new process
+		JobNode jobNode = this.office.createProcess(flowMetaData, parameter,
+				null, 0, null);
+
+		// Assign the job node to the Team
+		jobNode.activateJob();
+
+		// Indicate when thread of work complete
+		// TODO consider making this ProcessState so all threads complete
+		return jobNode.getFlow().getThreadState();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * net.officefloor.frame.api.manage.WorkManager#getManagedObject(java.lang
+	 * .String)
+	 */
+	public ManagedObject getManagedObject(String managedObjectId)
+			throws Exception {
+		// TODO implement
+		throw new UnsupportedOperationException("TODO implement");
+	}
+
+}
