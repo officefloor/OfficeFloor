5bf43753265f6680d4187fd3b7add00737aff5df
sagenschneider <sagenschneider@ee502a36-3d87-488a-bc83-848c733a9ae9>
2009-02-11 00:26:14 +0000

    Refactoring work so far:   - using scopes Process, Thread, Work   - using OfficeFloorIssues to capture issues (rather than failing on first)
    (saving work so far as going on vacation)

---
+++ officefloor/trunk/officeframe/src/main/impl/net/officefloor/frame/impl/execute/administrator/AdministratorIndexImpl.java
@@ -0,0 +1,81 @@
+/*
+ *  Office Floor, Application Server
+ *  Copyright (C) 2006 Daniel Sagenschneider
+ *
+ *  This program is free software; you can redistribute it and/or modify it under the terms 
+ *  of the GNU General Public License as published by the Free Software Foundation; either 
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with this program; 
+ *  if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *  MA 02111-1307 USA
+ */
+package net.officefloor.frame.impl.execute.administrator;
+
+import net.officefloor.frame.internal.structure.AdministratorIndex;
+import net.officefloor.frame.internal.structure.AdministratorScope;
+import net.officefloor.frame.spi.administration.Administrator;
+
+/**
+ * {@link AdministratorIndex} implementation.
+ * 
+ * @author Daniel
+ */
+public class AdministratorIndexImpl implements AdministratorIndex {
+
+	/**
+	 * {@link AdministratorScope}.
+	 */
+	private final AdministratorScope administratorScope;
+
+	/**
+	 * Index of the {@link Administrator} within the {@link AdministratorScope}.
+	 */
+	private final int indexOfAdministratorWithinScope;
+
+	/**
+	 * Initiate.
+	 * 
+	 * @param administratorScope
+	 *            {@link AdministratorScope}.
+	 * @param indexOfAdministratorWithinScope
+	 *            Index of the {@link Administrator} within the
+	 *            {@link AdministratorScope}.
+	 */
+	public AdministratorIndexImpl(AdministratorScope administratorScope,
+			int indexOfAdministratorWithinScope) {
+		this.administratorScope = administratorScope;
+		this.indexOfAdministratorWithinScope = indexOfAdministratorWithinScope;
+	}
+
+	/*
+	 * ================== AdministratorIndex =====================
+	 */
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @seenet.officefloor.frame.internal.structure.AdministratorIndex#
+	 * getAdministratorScope()
+	 */
+	@Override
+	public AdministratorScope getAdministratorScope() {
+		return this.administratorScope;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @seenet.officefloor.frame.internal.structure.AdministratorIndex#
+	 * getIndexOfAdministratorWithinScope()
+	 */
+	@Override
+	public int getIndexOfAdministratorWithinScope() {
+		return this.indexOfAdministratorWithinScope;
+	}
+
+}
---
+++ officefloor/trunk/officeframe/src/main/impl/net/officefloor/frame/impl/execute/administrator/ExtensionInterfaceMetaDataImpl.java
@@ -0,0 +1,84 @@
+/*
+ *  Office Floor, Application Server
+ *  Copyright (C) 2006 Daniel Sagenschneider
+ *
+ *  This program is free software; you can redistribute it and/or modify it under the terms 
+ *  of the GNU General Public License as published by the Free Software Foundation; either 
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with this program; 
+ *  if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *  MA 02111-1307 USA
+ */
+package net.officefloor.frame.impl.execute.administrator;
+
+import net.officefloor.frame.internal.structure.ExtensionInterfaceMetaData;
+import net.officefloor.frame.spi.managedobject.ManagedObject;
+import net.officefloor.frame.spi.managedobject.extension.ExtensionInterfaceFactory;
+
+/**
+ * Implementationg of the {@link ExtensionInterfaceMetaData}.
+ * 
+ * @author Daniel
+ */
+public class ExtensionInterfaceMetaDataImpl<I extends Object> implements
+		ExtensionInterfaceMetaData<I> {
+
+	/**
+	 * Index identifying the {@link ManagedObject} implementing the extension
+	 * interface.
+	 */
+	protected final int managedObjectIndex;
+
+	/**
+	 * {@link ExtensionInterfaceFactory} to create the extension interface from
+	 * the {@link ManagedObject}.
+	 */
+	protected final ExtensionInterfaceFactory<I> extensionInterfaceFactory;
+
+	/**
+	 * Initiate.
+	 * 
+	 * @param managedObjectIndex
+	 *            Index identifying the {@link ManagedObject} implementing the
+	 *            extension interface.
+	 * @param extensionInterfaceFactory
+	 *            {@link ExtensionInterfaceFactory} to create the extension
+	 *            interface from the {@link ManagedObject}.
+	 */
+	public ExtensionInterfaceMetaDataImpl(int managedObjectIndex,
+			ExtensionInterfaceFactory<I> extensionInterfaceFactory) {
+		// Store state
+		this.managedObjectIndex = managedObjectIndex;
+		this.extensionInterfaceFactory = extensionInterfaceFactory;
+	}
+
+	/*
+	 * ====================== ExtensionInterfaceMetaData =====================
+	 */
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @seenet.officefloor.frame.internal.structure.ExtensionInterfaceMetaData#
+	 * getManagedObjectKey()
+	 */
+	public int getManagedObjectIndex() {
+		return this.managedObjectIndex;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @seenet.officefloor.frame.internal.structure.ExtensionInterfaceMetaData#
+	 * getExtensionInterfaceFactory()
+	 */
+	public ExtensionInterfaceFactory<I> getExtensionInterfaceFactory() {
+		return this.extensionInterfaceFactory;
+	}
+
+}
