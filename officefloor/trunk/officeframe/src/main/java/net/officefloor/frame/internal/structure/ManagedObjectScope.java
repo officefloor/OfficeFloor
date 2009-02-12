4618f8df6cf6df53df5efb061dc979009ee6c767
sagenschneider <sagenschneider@ee502a36-3d87-488a-bc83-848c733a9ae9>
2009-02-12 18:24:26 +0000

    Refactoring work so far:   - using scopes Process, Thread, Work   - using OfficeFloorIssues to capture issues (rather than failing on first)
    (saving work so far as going on vacation)

---
+++ officefloor/trunk/officeframe/src/main/java/net/officefloor/frame/internal/structure/ManagedObjectScope.java
@@ -0,0 +1,42 @@
+/*
+ *  Office Floor, Application Server
+ *  Copyright (C) 2006 Daniel Sagenschneider
+ *
+ *  This program is free software; you can redistribute it and/or modify it under the terms 
+ *  of the GNU General Public License as published by the Free Software Foundation; either 
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with this program; 
+ *  if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *  MA 02111-1307 USA
+ */
+package net.officefloor.frame.internal.structure;
+
+import net.officefloor.frame.spi.managedobject.ManagedObject;
+
+/**
+ * Scopes for a {@link ManagedObject}.
+ * 
+ * @author Daniel
+ */
+public enum ManagedObjectScope {
+
+	/**
+	 * {@link ManagedObject} bound to {@link WorkContainer}.
+	 */
+	WORK,
+
+	/**
+	 * {@link ManagedObject} bound to {@link ThreadState}.
+	 */
+	THREAD,
+
+	/**
+	 * {@link ManagedObject} bound to {@link ProcessState}.
+	 */
+	PROCESS
+}
