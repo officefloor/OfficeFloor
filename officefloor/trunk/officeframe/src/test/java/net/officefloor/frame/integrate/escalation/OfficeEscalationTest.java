03ea99ef0b3c4f63c8d0d03a4f474dfcb0232871
Daniel <sagenschneider@users.noreply.github.com>
2009-03-13 16:58:06 +0000

    Unit tests passing after refactoring to have scopes for Managed Objects and Administrators.

--- officefloor/trunk/officeframe/src/test/java/net/officefloor/frame/integrate/escalation/OfficeEscalationTest.java
+++ officefloor/trunk/officeframe/src/test/java/net/officefloor/frame/integrate/escalation/OfficeEscalationTest.java
@@ -14,7 +14,7 @@
  *  if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
  *  MA 02111-1307 USA
  */
-package net.officefloor.frame.integrate;
+package net.officefloor.frame.integrate.escalation;
 
 import net.officefloor.frame.impl.spi.team.PassiveTeam;
 import net.officefloor.frame.internal.structure.ThreadState;
