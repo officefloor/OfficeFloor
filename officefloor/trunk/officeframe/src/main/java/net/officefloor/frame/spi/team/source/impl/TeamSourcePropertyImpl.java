2d7e314fc87d0053709e2abaa2c515f84d3062ad
Daniel <sagenschneider@users.noreply.github.com>
2009-03-13 21:26:00 +0000

    Removing TeamFactory to use TeamSource.

---
+++ officefloor/trunk/officeframe/src/main/java/net/officefloor/frame/spi/team/source/impl/TeamSourcePropertyImpl.java
@@ -0,0 +1,65 @@
+/*
+ *  Office Floor, Application Server
+ *  Copyright (C) 2006 Daniel Sagenschneider
+ *
+ *  This program is free software; you can redistribute it and/or modify it under the terms 
+ *  of the GNU General Public License as published by the Free Software Foundation; either 
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with this program; 
+ *  if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *  MA 02111-1307 USA
+ */
+package net.officefloor.frame.spi.team.source.impl;
+
+import net.officefloor.frame.spi.team.source.TeamSourceProperty;
+
+/**
+ * {@link TeamSourceProperty} implementation.
+ * 
+ * @author Daniel
+ */
+public class TeamSourcePropertyImpl implements TeamSourceProperty {
+
+	/**
+	 * Name of property.
+	 */
+	protected final String name;
+
+	/**
+	 * Label of property.
+	 */
+	protected final String label;
+
+	/**
+	 * Initiate with name and label of property.
+	 * 
+	 * @param name
+	 *            Name of property.
+	 * @param label
+	 *            Label of property.
+	 */
+	public TeamSourcePropertyImpl(String name, String label) {
+		this.name = name;
+		this.label = label;
+	}
+
+	/*
+	 * =============== TeamSourceProperty =============================
+	 */
+
+	@Override
+	public String getName() {
+		return this.name;
+	}
+
+	@Override
+	public String getLabel() {
+		return this.label;
+	}
+
+}
