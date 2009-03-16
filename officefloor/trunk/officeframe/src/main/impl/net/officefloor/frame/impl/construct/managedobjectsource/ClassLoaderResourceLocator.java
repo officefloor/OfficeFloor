458fb59f76ae3b6010df3e5acfc778178db09492
Daniel <sagenschneider@users.noreply.github.com>
2009-03-16 15:07:40 +0000

    Making packages visible for Eclipse plug-ins.

--- officefloor/trunk/officeframe/src/main/impl/net/officefloor/frame/impl/construct/managedobjectsource/ClassLoaderResourceLocator.java
+++ officefloor/trunk/officeframe/src/main/impl/net/officefloor/frame/impl/construct/managedobjectsource/ClassLoaderResourceLocator.java
@@ -22,52 +22,45 @@ import java.net.URL;
 import net.officefloor.frame.spi.managedobject.source.ResourceLocator;
 
 /**
- * Implementation of
- * {@link net.officefloor.frame.spi.managedobject.source.ResourceLocator} that
- * relies on a {@link java.lang.ClassLoader}.
+ * Implementation of {@link ResourceLocator} that relies on a
+ * {@link ClassLoader}.
  * 
  * @author Daniel
  */
 public class ClassLoaderResourceLocator implements ResourceLocator {
 
 	/**
-	 * {@link java.lang.ClassLoader} to locate resources.
+	 * {@link ClassLoader} to locate resources.
 	 */
-	protected final ClassLoader classLoader;
+	private final ClassLoader classLoader;
 
 	/**
 	 * Initiate.
 	 */
 	public ClassLoaderResourceLocator() {
-		// Store state
 		this.classLoader = this.getClass().getClassLoader();
 	}
 
 	/**
-	 * Initiate with the {@link java.lang.ClassLoader} to locate resources.
+	 * Initiate.
 	 * 
 	 * @param classLoader
-	 *            {@link java.lang.ClassLoader} to locate resources.
+	 *            {@link ClassLoader} to locate resources.
 	 */
 	public ClassLoaderResourceLocator(ClassLoader classLoader) {
-		// Store state
 		this.classLoader = classLoader;
 	}
 
 	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see net.officefloor.frame.spi.managedobject.source.ResourceLocator#locateInputStream(java.lang.String)
+	 * =================== ResourceLocator ===============================
 	 */
+
+	@Override
 	public InputStream locateInputStream(String name) {
 		return this.classLoader.getResourceAsStream(name);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see net.officefloor.frame.spi.managedobject.source.ResourceLocator#locateURL(java.lang.String)
-	 */
+	@Override
 	public URL locateURL(String name) {
 		return this.classLoader.getResource(name);
 	}
