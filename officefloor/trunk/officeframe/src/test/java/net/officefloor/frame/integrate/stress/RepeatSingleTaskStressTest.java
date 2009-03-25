af7132c6a4edc9b98fa2e198838c79edc77d9e59
sagenschneider <sagenschneider@ee502a36-3d87-488a-bc83-848c733a9ae9>
2009-03-25 00:01:29 +0000

    Adding stress tests.

---
+++ officefloor/trunk/officeframe/src/test/java/net/officefloor/frame/integrate/stress/RepeatSingleTaskStressTest.java
@@ -0,0 +1,122 @@
+/*
+ *  Office Floor, Application Server
+ *  Copyright (C) 2006 Daniel Sagenschneider
+ *
+ *  This program is free software; you can redistribute it and/or modify it under the terms 
+ *  of the GNU General Public License as published by the Free Software Foundation; either 
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with this program; 
+ *  if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *  MA 02111-1307 USA
+ */
+package net.officefloor.frame.integrate.stress;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+import java.lang.management.MemoryUsage;
+
+import net.officefloor.frame.api.execute.Task;
+import net.officefloor.frame.api.execute.TaskContext;
+import net.officefloor.frame.api.execute.Work;
+import net.officefloor.frame.impl.spi.team.OnePersonTeam;
+import net.officefloor.frame.test.AbstractOfficeConstructTestCase;
+
+/**
+ * Tests running the same {@link Task} many times.
+ * 
+ * @author Daniel
+ */
+public class RepeatSingleTaskStressTest extends AbstractOfficeConstructTestCase {
+
+	/**
+	 * Ensures no issues arising in stress repeating a {@link Task}.
+	 */
+	public void testStressRepeat() throws Exception {
+
+		int REPEAT_COUNT = 10000000;
+
+		// Create the repeat
+		RepeatTask repeat = new RepeatTask(REPEAT_COUNT);
+
+		// Register the repeat task
+		this.constructWork(repeat, "work", "repeat")
+				.buildTask("repeat", "TEAM").buildTaskContext();
+		this.constructTeam("TEAM", new OnePersonTeam(100));
+
+		// Run the repeats
+		this.invokeWork("work", null, 30);
+
+		// Ensure correct number of repeats
+		assertEquals("Incorrect number of repeats", REPEAT_COUNT, repeat
+				.getRepeatCount());
+	}
+
+	/**
+	 * {@link Work}.
+	 */
+	public static class RepeatTask {
+
+		/**
+		 * Number of times to repeat.
+		 */
+		private final int maxRepeatCalls;
+
+		/**
+		 * Number of times repeat called.
+		 */
+		private int repeatCount;
+
+		/**
+		 * Initiate.
+		 * 
+		 * @param maxRepeatCalls
+		 *            Number of times to repeat.
+		 */
+		public RepeatTask(int maxRepeatCalls) {
+			this.maxRepeatCalls = maxRepeatCalls;
+		}
+
+		/**
+		 * Obtains the number of times repeated.
+		 */
+		public synchronized int getRepeatCount() {
+			return this.repeatCount;
+		}
+
+		/**
+		 * Repeating task.
+		 * 
+		 * @param context
+		 *            {@link TaskContext}.
+		 */
+		public synchronized void repeat(TaskContext<?, ?, ?> context) {
+
+			// Determine if repeated enough
+			if (this.repeatCount >= this.maxRepeatCalls) {
+				return; // repeated enough
+			}
+
+			// Set up to repeat again
+			this.repeatCount++;
+			context.setComplete(false);
+
+			// Output heap sizes after garbage collection
+			if ((this.repeatCount % 100000) == 0) {
+				MemoryMXBean memoryBean = ManagementFactory.getMemoryMXBean();
+				memoryBean.gc();
+				memoryBean.gc();
+				MemoryUsage heap = memoryBean.getHeapMemoryUsage();
+				System.out.println("HEAP: "
+						+ (heap.getUsed() / (double) heap.getMax())
+						+ " finalize queue count "
+						+ memoryBean.getObjectPendingFinalizationCount());
+			}
+		}
+	}
+
+}
