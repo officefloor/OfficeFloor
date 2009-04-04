e8615fecdd65e52247f1d58d7cf6d0afaf2de414
sagenschneider <sagenschneider@ee502a36-3d87-488a-bc83-848c733a9ae9>
2009-04-04 09:59:11 +0000

    Tests added for DeskOperations (except conform operations).

---
+++ officefloor/trunk/officecompiler/src/test/java/net/officefloor/compile/impl/desk/AddExternalFlowTest.java
@@ -0,0 +1,67 @@
+/*
+ *  Office Floor, Application Server
+ *  Copyright (C) 2006 Daniel Sagenschneider
+ *
+ *  This program is free software; you can redistribute it and/or modify it under the terms 
+ *  of the GNU General Public License as published by the Free Software Foundation; either 
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with this program; 
+ *  if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *  MA 02111-1307 USA
+ */
+package net.officefloor.compile.impl.desk;
+
+import net.officefloor.compile.change.Change;
+import net.officefloor.model.desk.ExternalFlowModel;
+
+/**
+ * Tests adding an {@link ExternalFlowModel}.
+ * 
+ * @author Daniel
+ */
+public class AddExternalFlowTest extends AbstractDeskOperationsTestCase {
+
+	/**
+	 * Ensures that can add an {@link ExternalFlowModel}.
+	 */
+	public void testAddExternalFlow() {
+		Change<ExternalFlowModel> change = this.operations.addExternalFlow(
+				"FLOW", String.class.getName());
+		this.assertChange(change, null, "Add external flow FLOW", true);
+		change.apply();
+		assertEquals("Incorrect target", this.desk.getExternalFlows().get(0),
+				change.getTarget());
+	}
+
+	/**
+	 * Ensure ordering of {@link ExternalFlowModel} instances.
+	 */
+	public void testAddMultipleExternalFlowsEnsuringOrdering() {
+
+		// Create the changes
+		Change<ExternalFlowModel> changeB = this.operations.addExternalFlow(
+				"FLOW_B", Integer.class.getName());
+		Change<ExternalFlowModel> changeA = this.operations.addExternalFlow(
+				"FLOW_A", String.class.getName());
+		Change<ExternalFlowModel> changeC = this.operations.addExternalFlow(
+				"FLOW_C", Object.class.getName());
+
+		// Add the external flows
+		changeB.apply();
+		changeA.apply();
+		changeC.apply();
+		this.validateDesk();
+
+		// Revert
+		changeC.revert();
+		changeA.revert();
+		changeB.revert();
+		this.validateAsSetupDesk();
+	}
+
+}
---
+++ officefloor/trunk/officecompiler/src/test/java/net/officefloor/compile/impl/desk/SetTaskAsPublicTest.java
@@ -0,0 +1,74 @@
+/*
+ *  Office Floor, Application Server
+ *  Copyright (C) 2006 Daniel Sagenschneider
+ *
+ *  This program is free software; you can redistribute it and/or modify it under the terms 
+ *  of the GNU General Public License as published by the Free Software Foundation; either 
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with this program; 
+ *  if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *  MA 02111-1307 USA
+ */
+package net.officefloor.compile.impl.desk;
+
+import net.officefloor.compile.change.Change;
+import net.officefloor.model.desk.TaskModel;
+
+/**
+ * Tests setting the {@link TaskModel} as public.
+ * 
+ * @author Daniel
+ */
+public class SetTaskAsPublicTest extends AbstractDeskOperationsTestCase {
+
+	/**
+	 * Public {@link TaskModel}.
+	 */
+	private TaskModel publicTask;
+
+	/**
+	 * Private {@link TaskModel}.
+	 */
+	private TaskModel privateTask;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * net.officefloor.compile.impl.desk.AbstractDeskOperationsTestCase#setUp()
+	 */
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+
+		// Obtain the public and private tasks
+		this.publicTask = this.desk.getTasks().get(0);
+		this.privateTask = this.desk.getTasks().get(1);
+	}
+
+	/**
+	 * Ensures can set a {@link TaskModel} to be public.
+	 */
+	public void testTaskPublic() {
+		Change<TaskModel> change = this.operations.setTaskAsPublic(true,
+				this.privateTask);
+		this.assertChange(change, this.privateTask, "Set task PRIVATE public",
+				true);
+	}
+
+	/**
+	 * Ensures can set a {@link TaskModel} to be private.
+	 */
+	public void testSetTaskPrivate() {
+		Change<TaskModel> change = this.operations.setTaskAsPublic(false,
+				this.publicTask);
+		this.assertChange(change, this.publicTask, "Set task PUBLIC private",
+				true);
+	}
+
+}
