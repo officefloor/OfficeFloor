354ca4a1dcce27cf3fbfc55e9f18323907ff29df
Daniel <sagenschneider@users.noreply.github.com>
2009-04-05 18:53:43 +0000

    Able to load specification of a Section.

---
+++ officefloor/trunk/officecompiler/src/main/impl/net/officefloor/compile/impl/section/SectionLoaderImpl.java
@@ -0,0 +1,199 @@
+/*
+ *  Office Floor, Application Server
+ *  Copyright (C) 2006 Daniel Sagenschneider
+ *
+ *  This program is free software; you can redistribute it and/or modify it under the terms 
+ *  of the GNU General Public License as published by the Free Software Foundation; either 
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with this program; 
+ *  if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *  MA 02111-1307 USA
+ */
+package net.officefloor.compile.impl.section;
+
+import net.officefloor.compile.impl.properties.PropertyListImpl;
+import net.officefloor.compile.impl.util.CompileUtil;
+import net.officefloor.compile.issues.CompilerIssues;
+import net.officefloor.compile.issues.CompilerIssues.LocationType;
+import net.officefloor.compile.properties.PropertyList;
+import net.officefloor.compile.section.SectionLoader;
+import net.officefloor.compile.section.SectionType;
+import net.officefloor.compile.spi.section.Section;
+import net.officefloor.compile.spi.section.source.SectionSource;
+import net.officefloor.compile.spi.section.source.SectionSourceProperty;
+import net.officefloor.compile.spi.section.source.SectionSourceSpecification;
+import net.officefloor.model.section.SectionModel;
+
+/**
+ * {@link SectionLoader} implementation.
+ * 
+ * @author Daniel
+ */
+public class SectionLoaderImpl implements SectionLoader {
+
+	/**
+	 * Location of the {@link Section}.
+	 */
+	private final String sectionLocation;
+
+	/**
+	 * Initiate.
+	 * 
+	 * @param sectionLocation
+	 *            Location of the {@link SectionModel}.
+	 */
+	public SectionLoaderImpl(String sectionLocation) {
+		this.sectionLocation = sectionLocation;
+	}
+
+	/*
+	 * ====================== SectionLoader ====================================
+	 */
+
+	@Override
+	public <S extends SectionSource> PropertyList loadSpecification(
+			Class<S> sectionSourceClass, CompilerIssues issues) {
+
+		// Instantiate the section source
+		SectionSource sectionSource = CompileUtil.newInstance(
+				sectionSourceClass, SectionSource.class, LocationType.SECTION,
+				this.sectionLocation, null, null, issues);
+		if (sectionSource == null) {
+			return null; // failed to instantiate
+		}
+
+		// Obtain the specification
+		SectionSourceSpecification specification;
+		try {
+			specification = sectionSource.getSpecification();
+		} catch (Throwable ex) {
+			this.addIssue("Failed to obtain "
+					+ SectionSourceSpecification.class.getSimpleName()
+					+ " from " + sectionSourceClass.getName(), ex, issues);
+			return null; // failed to obtain
+		}
+
+		// Ensure have specification
+		if (specification == null) {
+			this.addIssue("No "
+					+ SectionSourceSpecification.class.getSimpleName()
+					+ " returned from " + sectionSourceClass.getName(), issues);
+			return null; // no specification obtained
+		}
+
+		// Obtain the properties
+		SectionSourceProperty[] sectionProperties;
+		try {
+			sectionProperties = specification.getProperties();
+		} catch (Throwable ex) {
+			this.addIssue("Failed to obtain "
+					+ SectionSourceProperty.class.getSimpleName()
+					+ " instances from "
+					+ SectionSourceSpecification.class.getSimpleName()
+					+ " for " + sectionSourceClass.getName(), ex, issues);
+			return null; // failed to obtain properties
+		}
+
+		// Load the section properties into a property list
+		PropertyList propertyList = new PropertyListImpl();
+		if (sectionProperties != null) {
+			for (int i = 0; i < sectionProperties.length; i++) {
+				SectionSourceProperty sectionProperty = sectionProperties[i];
+
+				// Ensure have the section property
+				if (sectionProperty == null) {
+					this.addIssue(SectionSourceProperty.class.getSimpleName()
+							+ " " + i + " is null from "
+							+ SectionSourceSpecification.class.getSimpleName()
+							+ " for " + sectionSourceClass.getName(), issues);
+					return null; // must have complete property details
+				}
+
+				// Obtain the property name
+				String name;
+				try {
+					name = sectionProperty.getName();
+				} catch (Throwable ex) {
+					this.addIssue("Failed to get name for "
+							+ SectionSourceProperty.class.getSimpleName() + " "
+							+ i + " from "
+							+ SectionSourceSpecification.class.getSimpleName()
+							+ " for " + sectionSourceClass.getName(), ex,
+							issues);
+					return null; // must have complete property details
+				}
+				if (CompileUtil.isBlank(name)) {
+					this.addIssue(SectionSourceProperty.class.getSimpleName()
+							+ " " + i + " provided blank name from "
+							+ SectionSourceSpecification.class.getSimpleName()
+							+ " for " + sectionSourceClass.getName(), issues);
+					return null; // must have complete property details
+				}
+
+				// Obtain the property label
+				String label;
+				try {
+					label = sectionProperty.getLabel();
+				} catch (Throwable ex) {
+					this.addIssue("Failed to get label for "
+							+ SectionSourceProperty.class.getSimpleName() + " "
+							+ i + " (" + name + ") from "
+							+ SectionSourceSpecification.class.getSimpleName()
+							+ " for " + sectionSourceClass.getName(), ex,
+							issues);
+					return null; // must have complete property details
+				}
+
+				// Add to the properties
+				propertyList.addProperty(name, label);
+			}
+		}
+
+		// Return the property list
+		return propertyList;
+	}
+
+	@Override
+	public <S extends SectionSource> SectionType loadSectionType(
+			Class<S> sectionSourceClass, PropertyList propertyList,
+			ClassLoader classLoader, CompilerIssues issues) {
+		// TODO Implement
+		throw new UnsupportedOperationException(
+				"TODO implement SectionLoader.loadSectionType");
+	}
+
+	/**
+	 * Adds an issue.
+	 * 
+	 * @param issueDescription
+	 *            Description of the issue.
+	 * @param issues
+	 *            {@link CompilerIssues}.
+	 */
+	private void addIssue(String issueDescription, CompilerIssues issues) {
+		issues.addIssue(LocationType.SECTION, this.sectionLocation, null, null,
+				issueDescription);
+	}
+
+	/**
+	 * Adds an issue.
+	 * 
+	 * @param issueDescription
+	 *            Description of the issue.
+	 * @param cause
+	 *            Cause of the issue.
+	 * @param issues
+	 *            {@link CompilerIssues}.
+	 */
+	private void addIssue(String issueDescription, Throwable cause,
+			CompilerIssues issues) {
+		issues.addIssue(LocationType.SECTION, this.sectionLocation, null, null,
+				issueDescription, cause);
+	}
+
+}
