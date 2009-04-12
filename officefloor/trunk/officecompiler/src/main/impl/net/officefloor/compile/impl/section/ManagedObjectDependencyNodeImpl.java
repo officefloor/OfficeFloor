ccca1ff9b6991fb76bc1f8d235ca7dee67a68741
sagenschneider <sagenschneider@ee502a36-3d87-488a-bc83-848c733a9ae9>
2009-04-12 09:19:39 +0000

    Adding SectionManagedObject now passing unit tests.

---
+++ officefloor/trunk/officecompiler/src/main/impl/net/officefloor/compile/impl/section/ManagedObjectDependencyNodeImpl.java
@@ -0,0 +1,107 @@
+/*
+ *  Office Floor, Application Server
+ *  Copyright (C) 2006 Daniel Sagenschneider
+ *
+ *  This program is free software; you can redistribute it and/or modify it under the terms 
+ *  of the GNU General Public License as published by the Free Software Foundation; either 
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with this program; 
+ *  if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *  MA 02111-1307 USA
+ */
+package net.officefloor.compile.impl.section;
+
+import net.officefloor.compile.internal.structure.LinkObjectNode;
+import net.officefloor.compile.internal.structure.ManagedObjectDependencyNode;
+import net.officefloor.compile.issues.CompilerIssues;
+import net.officefloor.compile.issues.CompilerIssues.LocationType;
+import net.officefloor.compile.spi.office.OfficeSection;
+import net.officefloor.compile.spi.section.ManagedObjectDependency;
+
+/**
+ * {@link ManagedObjectDependencyNode} implementation.
+ * 
+ * @author Daniel
+ */
+public class ManagedObjectDependencyNodeImpl implements
+		ManagedObjectDependencyNode {
+
+	/**
+	 * Name of this {@link ManagedObjectDependency}.
+	 */
+	private final String dependencyName;
+
+	/**
+	 * Location of the {@link OfficeSection} containing this
+	 * {@link ManagedObjectDependencyNode}.
+	 */
+	private final String sectionLocation;
+
+	/**
+	 * {@link CompilerIssues}.
+	 */
+	private final CompilerIssues issues;
+
+	/**
+	 * Initiate.
+	 * 
+	 * @param dependencyName
+	 *            Name of this {@link ManagedObjectDependency}.
+	 * @param sectionLocation
+	 *            Location of the {@link OfficeSection} containing this
+	 *            {@link ManagedObjectDependencyNode}.
+	 * @param issues
+	 *            {@link CompilerIssues}.
+	 */
+	public ManagedObjectDependencyNodeImpl(String dependencyName,
+			String sectionLocation, CompilerIssues issues) {
+		this.dependencyName = dependencyName;
+		this.sectionLocation = sectionLocation;
+		this.issues = issues;
+	}
+
+	/*
+	 * ==================== ManagedObjectDependency ============================
+	 */
+
+	@Override
+	public String getManagedObjectDependencyName() {
+		return this.dependencyName;
+	}
+
+	/*
+	 * ===================== LinkObjectNode ===========================
+	 */
+
+	/**
+	 * Linked {@link LinkObjectNode}.
+	 */
+	private LinkObjectNode linkedObjectNode;
+
+	@Override
+	public boolean linkObjectNode(LinkObjectNode node) {
+
+		// Ensure not already linked
+		if (this.linkedObjectNode != null) {
+			this.issues.addIssue(LocationType.SECTION, this.sectionLocation,
+					null, null, "Managed object dependency "
+							+ this.dependencyName + " linked more than once");
+			return false; // already linked
+		}
+
+		// Link
+		this.linkedObjectNode = node;
+		return true;
+	}
+
+	@Override
+	public LinkObjectNode getLinkedObjectNode() {
+		return this.linkedObjectNode;
+	}
+
+}
