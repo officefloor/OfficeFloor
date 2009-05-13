00a3be924f556749cdeaf619352c3a4db52594b9
Daniel <sagenschneider@users.noreply.github.com>
2009-05-13 17:17:29 +0000

    Removing tests from OfficeEclipse (should run as PDE rather than trying to mimic it).
    Fixing up the socket managed object source extension.

--- officefloor/trunk/officeeclipse_socket/src/main/java/net/officefloor/eclipse/socket/HttpRouteWorkSourceExtension.java
+++ officefloor/trunk/officeeclipse_socket/src/main/java/net/officefloor/eclipse/socket/HttpRouteWorkSourceExtension.java
@@ -16,68 +16,49 @@
  */
 package net.officefloor.eclipse.socket;
 
-import java.util.LinkedList;
 import java.util.List;
 
+import net.officefloor.compile.properties.PropertyList;
 import net.officefloor.eclipse.common.dialog.input.InputHandler;
 import net.officefloor.eclipse.common.dialog.input.InputListener;
 import net.officefloor.eclipse.common.dialog.input.impl.BeanListInput;
 import net.officefloor.eclipse.extension.classpath.ClasspathProvision;
 import net.officefloor.eclipse.extension.classpath.ExtensionClasspathProvider;
 import net.officefloor.eclipse.extension.classpath.TypeClasspathProvision;
-import net.officefloor.eclipse.extension.workloader.WorkLoaderExtension;
-import net.officefloor.eclipse.extension.workloader.WorkLoaderExtensionContext;
-import net.officefloor.eclipse.extension.workloader.WorkLoaderProperty;
-import net.officefloor.work.WorkLoader;
-import net.officefloor.work.http.route.HttpRouteWorkLoader;
+import net.officefloor.eclipse.extension.worksource.WorkSourceExtension;
+import net.officefloor.eclipse.extension.worksource.WorkSourceExtensionContext;
+import net.officefloor.plugin.work.http.route.HttpRouteTask;
+import net.officefloor.plugin.work.http.route.HttpRouteWorkSource;
 
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 
 /**
- * Http Route {@link WorkLoaderExtension}.
+ * {@link WorkSourceExtension} for the {@link HttpRouteWorkSource}.
  * 
  * @author Daniel
  */
-public class HttpRouteWorkLoaderExtension implements WorkLoaderExtension,
+public class HttpRouteWorkSourceExtension implements
+		WorkSourceExtension<HttpRouteTask, HttpRouteWorkSource>,
 		ExtensionClasspathProvider {
 
 	/*
 	 * ====================== WorkLoaderExtension =========================
 	 */
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @seenet.officefloor.eclipse.extension.workloader.WorkLoaderExtension#
-	 * getWorkLoaderClass()
-	 */
 	@Override
-	public Class<? extends WorkLoader> getWorkLoaderClass() {
-		return HttpRouteWorkLoader.class;
+	public Class<HttpRouteWorkSource> getWorkSourceClass() {
+		return HttpRouteWorkSource.class;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @seenet.officefloor.eclipse.extension.workloader.WorkLoaderExtension#
-	 * getDisplayName()
-	 */
 	@Override
-	public String getDisplayName() {
+	public String getWorkSourceLabel() {
 		return "HTTP Route";
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @seenet.officefloor.eclipse.extension.workloader.WorkLoaderExtension#
-	 * createControl(org.eclipse.swt.widgets.Composite,
-	 * net.officefloor.eclipse.extension.workloader.WorkLoaderExtensionContext)
-	 */
 	@Override
-	public List<WorkLoaderProperty> createControl(Composite page,
-			final WorkLoaderExtensionContext context) {
+	public void createControl(Composite page,
+			final WorkSourceExtensionContext context) {
 
 		// Specify layout of page
 		page.setLayout(new GridLayout(1, false));
@@ -95,14 +76,14 @@ public class HttpRouteWorkLoaderExtension implements WorkLoaderExtension,
 				List<RoutingEntry> routingEntries = (List<RoutingEntry>) value;
 
 				// Create the listing of properties
-				List<WorkLoaderProperty> workLoaderProperties = new LinkedList<WorkLoaderProperty>();
+				PropertyList properties = context.getPropertyList();
+				properties.clear();
 				for (RoutingEntry routingEntry : routingEntries) {
-					workLoaderProperties.add(routingEntry
-							.createWorkLoaderProperty());
+					routingEntry.loadProperty(properties);
 				}
 
 				// Notify of change
-				context.notifyPropertiesChanged(workLoaderProperties);
+				context.notifyPropertiesChanged();
 			}
 
 			@Override
@@ -110,19 +91,10 @@ public class HttpRouteWorkLoaderExtension implements WorkLoaderExtension,
 				context.setErrorMessage(message);
 			}
 		});
-
-		// Initially no routing entries
-		return null;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @seenet.officefloor.eclipse.extension.workloader.WorkLoaderExtension#
-	 * getSuggestedWorkName(java.util.List)
-	 */
 	@Override
-	public String getSuggestedWorkName(List<WorkLoaderProperty> properties) {
+	public String getSuggestedWorkName(PropertyList properties) {
 		return "HttpRouter";
 	}
 
@@ -130,17 +102,10 @@ public class HttpRouteWorkLoaderExtension implements WorkLoaderExtension,
 	 * =================== ExtensionClasspathProvider =====================
 	 */
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * net.officefloor.eclipse.extension.classpath.ExtensionClasspathProvider
-	 * #getClasspathProvisions()
-	 */
 	@Override
 	public ClasspathProvision[] getClasspathProvisions() {
 		return new ClasspathProvision[] { new TypeClasspathProvision(
-				HttpRouteWorkLoader.class) };
+				HttpRouteWorkSource.class) };
 	}
 
 }
