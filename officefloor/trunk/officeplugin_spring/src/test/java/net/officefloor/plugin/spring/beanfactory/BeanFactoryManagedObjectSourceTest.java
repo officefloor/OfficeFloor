bbbaad35051b3cc79d9f136f9865da9669d40ac1
sagenschneider <sagenschneider@ee502a36-3d87-488a-bc83-848c733a9ae9>
2009-05-16 10:39:02 +0000

    Able to provide means to migrate Spring applications to OfficeFloor.

--- officefloor/trunk/officeplugin_spring/src/test/java/net/officefloor/plugin/spring/beanfactory/BeanFactoryManagedObjectSourceTest.java
+++ officefloor/trunk/officeplugin_spring/src/test/java/net/officefloor/plugin/spring/beanfactory/BeanFactoryManagedObjectSourceTest.java
@@ -16,10 +16,15 @@
  */
 package net.officefloor.plugin.spring.beanfactory;
 
+import java.sql.Connection;
+
 import net.officefloor.compile.managedobject.ManagedObjectType;
 import net.officefloor.compile.test.managedobject.ManagedObjectLoaderUtil;
 import net.officefloor.compile.test.managedobject.ManagedObjectTypeBuilder;
+import net.officefloor.frame.api.build.Indexed;
+import net.officefloor.frame.spi.managedobject.CoordinatingManagedObject;
 import net.officefloor.frame.spi.managedobject.ManagedObject;
+import net.officefloor.frame.spi.managedobject.ObjectRegistry;
 import net.officefloor.frame.test.OfficeFrameTestCase;
 import net.officefloor.frame.util.ManagedObjectSourceStandAlone;
 import net.officefloor.frame.util.ManagedObjectUserStandAlone;
@@ -53,6 +58,7 @@ public class BeanFactoryManagedObjectSourceTest extends OfficeFrameTestCase {
 		ManagedObjectTypeBuilder type = ManagedObjectLoaderUtil
 				.createManagedObjectTypeBuilder();
 		type.setObjectClass(BeanFactory.class);
+		type.addDependency("testFactory", Connection.class, 0, null);
 
 		// Validate the managed object type
 		ManagedObjectLoaderUtil.validateManagedObjectType(type,
@@ -85,6 +91,62 @@ public class BeanFactoryManagedObjectSourceTest extends OfficeFrameTestCase {
 		BeanFactory beanFactory = (BeanFactory) object;
 		TestBean bean = (TestBean) beanFactory.getBean("test", TestBean.class);
 		assertNotNull("Incorrect bean factory", bean);
+	}
+
+	/**
+	 * Ensures able to obtain dependency via the {@link DependencyFactoryBean}.
+	 */
+	@SuppressWarnings("unchecked")
+	public void testObtainDependency() throws Throwable {
+
+		final ObjectRegistry<Indexed> objectRegistry = this
+				.createMock(ObjectRegistry.class);
+		final Connection DEPENDENCY = this.createMock(Connection.class);
+
+		// Record obtaining the dependency
+		this.recordReturn(objectRegistry, objectRegistry.getObject(0),
+				DEPENDENCY);
+		this.recordReturn(objectRegistry, objectRegistry.getObject(0),
+				DEPENDENCY);
+
+		// Record delegating functionality
+		this.recordReturn(DEPENDENCY, DEPENDENCY.getClientInfo("ONE"), "A");
+		this.recordReturn(DEPENDENCY, DEPENDENCY.getClientInfo("TWO"), "B");
+
+		this.replayMockObjects();
+
+		// Obtain the path to the configuration
+		String configurationPath = this.getBeanFactoryConfigurationPath();
+
+		// Ensure able to obtain the bean factory
+		ManagedObjectSourceStandAlone standAlone = new ManagedObjectSourceStandAlone();
+		standAlone.addProperty(
+				BeanFactoryManagedObjectSource.BEAN_FACTORY_PATH_PROPERTY_NAME,
+				configurationPath);
+		BeanFactoryManagedObjectSource managedObjectSource = standAlone
+				.loadManagedObjectSource(BeanFactoryManagedObjectSource.class);
+		CoordinatingManagedObject<Indexed> managedObject = (CoordinatingManagedObject<Indexed>) ManagedObjectUserStandAlone
+				.sourceManagedObject(managedObjectSource);
+		BeanFactory beanFactory = (BeanFactory) managedObject.getObject();
+
+		// Ensure able to obtain dependency before coordinating
+		Connection beforeDependency = (Connection) beanFactory
+				.getBean("testFactory");
+
+		// Provide registry
+		managedObject.loadObjects(objectRegistry);
+
+		// Ensure able to obtain dependency after coordinating
+		Connection afterDependency = (Connection) beanFactory
+				.getBean("testFactory");
+
+		// Ensure delegate to actual dependency
+		assertEquals("Incorrect before delegation", "A", beforeDependency
+				.getClientInfo("ONE"));
+		assertEquals("Incorrect after delegation", "B", afterDependency
+				.getClientInfo("TWO"));
+
+		this.verifyMockObjects();
 	}
 
 	/**
