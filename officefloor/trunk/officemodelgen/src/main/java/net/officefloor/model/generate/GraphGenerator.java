d46fa91b372fabefc459e0d95b1dcd4f9ca44777
Daniel <sagenschneider@users.noreply.github.com>
2009-05-27 20:52:57 +0000

    Making auto directory available in repository.  Also fixing bug in model generator to stop removing the .svn directory of auto.

--- officefloor/trunk/officemodelgen/src/main/java/net/officefloor/model/generate/GraphGenerator.java
+++ officefloor/trunk/officemodelgen/src/main/java/net/officefloor/model/generate/GraphGenerator.java
@@ -2,16 +2,16 @@
  *  Office Floor, Application Server
  *  Copyright (C) 2006 Daniel Sagenschneider
  *
- *  This program is free software; you can redistribute it and/or modify it under the terms 
- *  of the GNU General Public License as published by the Free Software Foundation; either 
+ *  This program is free software; you can redistribute it and/or modify it under the terms
+ *  of the GNU General Public License as published by the Free Software Foundation; either
  *  version 2 of the License, or (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
- *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *  See the GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License along with this program; 
- *  if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *  You should have received a copy of the GNU General Public License along with this program;
+ *  if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
  *  MA 02111-1307 USA
  */
 package net.officefloor.model.generate;
@@ -24,7 +24,7 @@ import net.officefloor.model.repository.ConfigurationContext;
 
 /**
  * Generates all the models.
- * 
+ *
  * @author Daniel
  */
 public class GraphGenerator {
@@ -101,11 +101,16 @@ public class GraphGenerator {
 
 	/**
 	 * Clears the input directory.
-	 * 
+	 *
 	 * @param dir
 	 *            Directory to clear.
 	 */
 	private void deleteFile(File file) throws Exception {
+
+		// Ignore special files (starting with a '.')
+		if (file.getName().startsWith(".")) {
+			return;
+		}
 
 		// Clear the if directory
 		if (file.isDirectory()) {
