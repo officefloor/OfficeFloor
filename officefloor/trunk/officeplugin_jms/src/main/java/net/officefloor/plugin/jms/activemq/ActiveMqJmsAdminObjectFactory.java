f8d3e8432d2491414977f0190ca0de61e950523d
sagenschneider <sagenschneider@ee502a36-3d87-488a-bc83-848c733a9ae9>
2009-05-29 15:07:22 +0000

    Fixing licensing and updating to have full author name.

--- officefloor/trunk/officeplugin_jms/src/main/java/net/officefloor/plugin/jms/activemq/ActiveMqJmsAdminObjectFactory.java
+++ officefloor/trunk/officeplugin_jms/src/main/java/net/officefloor/plugin/jms/activemq/ActiveMqJmsAdminObjectFactory.java
@@ -1,5 +1,18 @@
 /*
- * Created on 2/03/2006
+ * Office Floor, Application Server
+ * Copyright (C) 2005-2009 Daniel Sagenschneider
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
  */
 package net.officefloor.plugin.jms.activemq;
 
@@ -14,8 +27,8 @@ import org.apache.activemq.command.ActiveMQDestination;
 
 /**
  * Factory for the creation of administered objects for ActiveMq.
- * 
- * @author Daniel
+ *
+ * @author Daniel Sagenschneider
  */
 public class ActiveMqJmsAdminObjectFactory implements JmsAdminObjectFactory {
 
--- officefloor/trunk/officeplugin_jms/src/test/java/net/officefloor/plugin/jms/AbstractJmsManagedObjectTest.java
+++ officefloor/trunk/officeplugin_jms/src/test/java/net/officefloor/plugin/jms/AbstractJmsManagedObjectTest.java
@@ -1,4 +1,20 @@
 /*
+ * Office Floor, Application Server
+ * Copyright (C) 2005-2009 Daniel Sagenschneider
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+/*
  * Created on 22/02/2006
  */
 package net.officefloor.plugin.jms;
@@ -24,7 +40,7 @@ import net.officefloor.plugin.jms.activemq.VmJmsAdminObjectFactory;
 /**
  * Abstract {@link TestCase} for testing JMS {@link ManagedObject}.
  * 
- * @author Daniel
+ * @author Daniel Sagenschneider
  */
 public abstract class AbstractJmsManagedObjectTest extends
 		AbstractOfficeConstructTestCase {
