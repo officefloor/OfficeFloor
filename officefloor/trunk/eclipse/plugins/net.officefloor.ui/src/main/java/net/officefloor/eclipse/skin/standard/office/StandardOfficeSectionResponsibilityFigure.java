2f46b405d9faf4cd499320e0e813400529b218f6
sagenschneider <sagenschneider@ee502a36-3d87-488a-bc83-848c733a9ae9>
2009-06-20 22:27:09 +0000

    - fixing licensing  - 0.0.1 beta version released  - demo available for website  - coming soon website released

--- officefloor/trunk/eclipse/plugins/net.officefloor.ui/src/main/java/net/officefloor/eclipse/skin/standard/office/StandardOfficeSectionResponsibilityFigure.java
+++ officefloor/trunk/eclipse/plugins/net.officefloor.ui/src/main/java/net/officefloor/eclipse/skin/standard/office/StandardOfficeSectionResponsibilityFigure.java
@@ -1,18 +1,19 @@
 /*
- * Office Floor, Application Server
+ * OfficeFloor - http://www.officefloor.net
  * Copyright (C) 2005-2009 Daniel Sagenschneider
  *
- * This program is free software; you can redistribute it and/or modify it under the terms
- * of the GNU General Public License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with this program;
- * if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- * MA 02111-1307 USA
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package net.officefloor.eclipse.skin.standard.office;
 
@@ -22,13 +23,13 @@ import net.officefloor.eclipse.skin.standard.AbstractOfficeFloorFigure;
 import net.officefloor.eclipse.skin.standard.StandardOfficeFloorColours;
 import net.officefloor.eclipse.skin.standard.figure.ConnectorFigure;
 import net.officefloor.eclipse.skin.standard.figure.LabelConnectorFigure;
+import net.officefloor.eclipse.skin.standard.figure.NoSpacingGridLayout;
 import net.officefloor.eclipse.skin.standard.figure.ConnectorFigure.ConnectorDirection;
 import net.officefloor.model.office.OfficeSectionResponsibilityModel;
 import net.officefloor.model.office.OfficeSectionResponsibilityToOfficeTeamModel;
 
 import org.eclipse.draw2d.ConnectionAnchor;
 import org.eclipse.draw2d.Figure;
-import org.eclipse.draw2d.GridLayout;
 import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.Label;
 
@@ -56,7 +57,7 @@ public class StandardOfficeSectionResponsibilityFigure extends
 			OfficeSectionResponsibilityFigureContext context) {
 
 		Figure container = new Figure();
-		container.setLayoutManager(new GridLayout(2, false));
+		container.setLayoutManager(new NoSpacingGridLayout(2));
 
 		// Create connector for managed objects
 		ConnectorFigure moConnector = new ConnectorFigure(
