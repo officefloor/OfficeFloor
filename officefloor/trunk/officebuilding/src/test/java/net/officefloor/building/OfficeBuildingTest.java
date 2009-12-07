5dd7c169104ff71021f1ed8e90de1dc1d8f19e47
sagenschneider <sagenschneider@ee502a36-3d87-488a-bc83-848c733a9ae9>
2009-12-07 12:49:44 +0000

    Adding commands to start/stop the OfficeBuilding.  Also providing command to obtain connection url.

--- officefloor/trunk/officebuilding/src/test/java/net/officefloor/building/OfficeBuildingTest.java
+++ officefloor/trunk/officebuilding/src/test/java/net/officefloor/building/OfficeBuildingTest.java
@@ -17,10 +17,18 @@
  */
 package net.officefloor.building;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.Reader;
+import java.lang.reflect.UndeclaredThrowableException;
 
 import junit.framework.TestCase;
-import net.officefloor.building.OfficeBuilding;
+import net.officefloor.building.manager.OfficeBuildingManager;
+import net.officefloor.building.manager.OfficeBuildingManagerMBean;
 
 /**
  * Tests the {@link OfficeBuilding}.
@@ -29,16 +37,49 @@ import net.officefloor.building.OfficeBuilding;
  */
 public class OfficeBuildingTest extends TestCase {
 
+	/**
+	 * Capture of <code>stdout</code>.
+	 */
+	private final ByteArrayOutputStream stdout = new ByteArrayOutputStream();
+
+	/**
+	 * <code>stdout</code> to reinstate after test.
+	 */
+	private PrintStream _stdout;
+
+	/**
+	 * Capture of <code>stderr</code>.
+	 */
+	private final ByteArrayOutputStream stderr = new ByteArrayOutputStream();
+
+	/**
+	 * <code>stderr</code> to reinstate after test.
+	 */
+	private PrintStream _stderr;
+
 	@Override
 	protected void setUp() throws Exception {
 		// Flag that testing
 		OfficeBuilding.isTesting = true;
+
+		// Collect output
+		this._stdout = System.out;
+		System.setOut(new PrintStream(this.stdout));
+		this._stderr = System.err;
+		System.setErr(new PrintStream(this.stderr));
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		// Reinstate output streams
+		System.setOut(this._stdout);
+		System.setErr(this._stderr);
 	}
 
 	/**
-	 * Ensure can start the {@link OfficeBuilding}.
+	 * Ensure can start and then stop the {@link OfficeBuilding}.
 	 */
-	public void testStartOfficeBuilding() throws Throwable {
+	public void testOfficeBuildingLifecycle_start_stop() throws Throwable {
 
 		// Provide Office Building Home
 		File officeBuildingHomeDir = new File(".", "src/main/resources");
@@ -46,7 +87,119 @@ public class OfficeBuildingTest extends TestCase {
 				officeBuildingHomeDir.getAbsolutePath());
 
 		// Start the Office Building
+		long beforeStartTime = System.currentTimeMillis();
 		OfficeBuilding.main("start");
+		long afterStartTime = System.currentTimeMillis();
+
+		// Ensure started
+		OfficeBuildingManagerMBean manager = OfficeBuildingManager
+				.getOfficeBuildingManager(null, 13778);
+		long startTime = manager.getStartTime().getTime();
+		assertTrue(
+				"Office Building should be just started",
+				((beforeStartTime <= startTime) && (startTime <= afterStartTime)));
+
+		// Stop the Office Building
+		OfficeBuilding.main("stop");
+
+		// Ensure stopped
+		try {
+			manager.getStartTime();
+			fail("Office Building should be stopped");
+		} catch (UndeclaredThrowableException ex) {
+			// Ensure cause is IO failure as Office Building stopped
+			Throwable cause = ex.getCause();
+			assertTrue("Incorrect cause", (cause instanceof IOException));
+		}
+	}
+
+	/**
+	 * Ensure able to run <code>url</code> command.
+	 */
+	public void testUrl() throws Throwable {
+
+		final String HOST = "server";
+		final int PORT = 13778;
+
+		// Obtain the URL
+		OfficeBuilding.main("url", HOST, String.valueOf(PORT));
+
+		// Validate output URL
+		String expectedUrl = OfficeBuildingManager
+				.getOfficeBuildingJmxServiceUrl(HOST, PORT).toString()
+				+ "\n";
+		validateStreamContent("Incorrect URL", expectedUrl, this.stdout);
+	}
+
+	/**
+	 * Ensure provides usage on no command.
+	 */
+	public void testUsageMessage() throws Throwable {
+
+		// Attempt with no command
+		try {
+			OfficeBuilding.main();
+			fail("Should not succeed as requires command");
+		} catch (Error ex) {
+			validateStreamContent("Should provide usage message",
+					OfficeBuilding.USAGE_MESSAGE + "\n", this.stderr);
+			assertEquals("Incorrect requires command cause", "Exit", ex
+					.getMessage());
+		}
+	}
+
+	/**
+	 * Ensure provides usage on unknown command.
+	 */
+	public void testUnknownCommand() throws Throwable {
+
+		// Attempt with unknown command
+		try {
+			OfficeBuilding.main("unknown");
+			fail("Should not succeed as unknown command");
+		} catch (Error ex) {
+			String errorMessage = "ERROR: unknown command 'unknown'\n\n"
+					+ OfficeBuilding.USAGE_MESSAGE + "\n";
+			validateStreamContent("Should provide usage message", errorMessage,
+					this.stderr);
+			assertEquals("Incorrect unknown command cause", "Exit", ex
+					.getMessage());
+		}
+	}
+
+	/**
+	 * Validates the contents of the stream.
+	 * 
+	 * @param message
+	 *            Message describing validation.
+	 * @param expectedContent
+	 *            Expected content of the stream.
+	 * @param stream
+	 *            Stream to validate its content.
+	 */
+	private static void validateStreamContent(String message,
+			String expectedContent, ByteArrayOutputStream stream)
+			throws Exception {
+		String actualContent = getStreamContent(stream);
+		assertEquals(message, expectedContent, actualContent);
+	}
+
+	/**
+	 * Obtains the contents of the stream.
+	 * 
+	 * @param stream
+	 *            Stream to obtain its contents.
+	 * @return Contents of the stream.
+	 */
+	private static String getStreamContent(ByteArrayOutputStream stream)
+			throws Exception {
+		Reader reader = new InputStreamReader(new ByteArrayInputStream(stream
+				.toByteArray()));
+		StringBuilder content = new StringBuilder();
+		for (int value = reader.read(); value != -1; value = reader.read()) {
+			content.append((char) value);
+		}
+		return content.toString();
 	}
 
 }
