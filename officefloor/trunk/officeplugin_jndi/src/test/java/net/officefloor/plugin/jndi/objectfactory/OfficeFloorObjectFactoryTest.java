d1a302657979441d92e9bbf183ac62b69134e4d4
sagenschneider <sagenschneider@ee502a36-3d87-488a-bc83-848c733a9ae9>
2010-02-18 14:38:11 +0000

    Moving tests appropriate to OfficeFloorContext.

--- officefloor/trunk/officeplugin_jndi/src/test/java/net/officefloor/plugin/jndi/objectfactory/OfficeFloorObjectFactoryTest.java
+++ officefloor/trunk/officeplugin_jndi/src/test/java/net/officefloor/plugin/jndi/objectfactory/OfficeFloorObjectFactoryTest.java
@@ -17,12 +17,6 @@
  */
 package net.officefloor.plugin.jndi.objectfactory;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-
-import net.officefloor.frame.api.manage.Office;
-import net.officefloor.frame.api.manage.OfficeFloor;
-import net.officefloor.frame.api.manage.WorkManager;
 import net.officefloor.frame.test.OfficeFrameTestCase;
 
 /**
@@ -32,35 +26,8 @@ import net.officefloor.frame.test.OfficeFrameTestCase;
  */
 public class OfficeFloorObjectFactoryTest extends OfficeFrameTestCase {
 
-	/**
-	 * Package name for this class.
-	 */
-	private final String packageName = this.getClass().getPackage().getName();
-
-	/**
-	 * Ensure that able to use JNDI to instantiate an {@link OfficeFloor}
-	 * instance by referencing the configuration file directly.
-	 */
-	public void testInstantiateOfficeFloorDirectly() throws Exception {
-
-		// Create the initial context
-		Context context = new InitialContext();
-
-		// Specify name of OfficeFloor
-		String name = "officefloor:" + this.packageName + "/direct";
-
-		// Obtain the OfficeFloor
-		Object object = context.lookup(name);
-		assertNotNull("No object looked up", object);
-		assertTrue("Incorrect object type", object instanceof OfficeFloor);
-		OfficeFloor officeFloor = (OfficeFloor) object;
-
-		// Invoke the work to ensure correct OfficeFloor
-		ValidateWork.reset();
-		Office office = officeFloor.getOffice("OFFICE");
-		WorkManager workManager = office.getWorkManager("SECTION.WORK");
-		workManager.invokeWork(null);
-		assertTrue("Task should be invoked", ValidateWork.isTaskInvoked());
+	public void testTODO() {
+		fail("TODO implement test");
 	}
 
 }
