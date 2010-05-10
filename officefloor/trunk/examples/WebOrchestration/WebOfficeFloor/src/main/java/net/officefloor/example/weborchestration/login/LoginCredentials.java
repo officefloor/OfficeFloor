c9b06368fdc2426d2786eb947d9c7535cd5073aa
sagenschneider <sagenschneider@ee502a36-3d87-488a-bc83-848c733a9ae9>
2010-05-10 14:07:53 +0000

    Incomplete - task 2983880: Web Orchestration example - https://sourceforge.net/tracker/?func=detail&group_id=156349&atid=799429&aid=2983880
    Able to get to login page.

---
+++ officefloor/trunk/examples/WebOrchestration/WebOfficeFloor/src/main/java/net/officefloor/example/weborchestration/login/LoginCredentials.java
@@ -0,0 +1,19 @@
+package net.officefloor.example.weborchestration.login;
+
+/**
+ * Login credentials.
+ * 
+ * @author daniel
+ */
+public class LoginCredentials {
+
+	/**
+	 * Obtains the error in login.
+	 * 
+	 * @return Error in login.
+	 */
+	public String getError() {
+		return null;
+	}
+
+}
