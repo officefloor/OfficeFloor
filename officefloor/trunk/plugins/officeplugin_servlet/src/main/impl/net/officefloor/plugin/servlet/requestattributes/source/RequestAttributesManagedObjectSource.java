585682acf61caceb863de197359f10232f3d09a5
Daniel <sagenschneider@users.noreply.github.com>
2010-10-10 13:12:54 +0000

    Providing Request Attributes ManagedObjectSource in its own java package.

--- officefloor/trunk/plugins/officeplugin_servlet/src/main/impl/net/officefloor/plugin/servlet/requestattributes/source/RequestAttributesManagedObjectSource.java
+++ officefloor/trunk/plugins/officeplugin_servlet/src/main/impl/net/officefloor/plugin/servlet/requestattributes/source/RequestAttributesManagedObjectSource.java
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package net.officefloor.plugin.servlet.container.source;
+package net.officefloor.plugin.servlet.requestattributes.source;
 
 import java.util.Collections;
 import java.util.HashMap;
