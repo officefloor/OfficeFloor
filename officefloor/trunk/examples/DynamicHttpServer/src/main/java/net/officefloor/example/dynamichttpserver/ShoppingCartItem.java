50e07c137f483cac8a897bf91f89291b2b24544f
Daniel <sagenschneider@users.noreply.github.com>
2011-01-29 07:09:29 +0000

    Complete - task 3165915: Example of dynamic web content for HTTP Server - https://sourceforge.net/tracker/?func=detail&group_id=156349&atid=7994293165915

---
+++ officefloor/trunk/examples/DynamicHttpServer/src/main/java/net/officefloor/example/dynamichttpserver/ShoppingCartItem.java
@@ -0,0 +1,53 @@
+/*
+ * OfficeFloor - http://www.officefloor.net
+ * Copyright (C) 2005-2011 Daniel Sagenschneider
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.officefloor.example.dynamichttpserver;
+
+/**
+ * Shopping cart bean.
+ * 
+ * @author Daniel Sagenschneider
+ */
+// START SNIPPET: example
+public class ShoppingCartItem {
+
+	private String name;
+
+	private double price;
+
+	private int quantity;
+
+	public ShoppingCartItem(String name, double price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public double getPrice() {
+		return this.price;
+	}
+
+	public int getQuantity() {
+		return this.quantity;
+	}
+
+}
+// END SNIPPET: example
