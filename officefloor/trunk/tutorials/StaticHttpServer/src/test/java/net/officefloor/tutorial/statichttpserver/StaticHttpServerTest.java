3c6057897dd718d370f7d0b71b2942ffa816139e
sagenschneider <sagenschneider@ee502a36-3d87-488a-bc83-848c733a9ae9>
2011-04-22 12:05:57 +0000

    Incomplete - task 3287604: Use WoOF configuration in examples - https://sourceforge.net/tracker/?func=detail&group_id=156349&atid=7994293287604
    Making as a tutorial.

--- officefloor/trunk/tutorials/StaticHttpServer/src/test/java/net/officefloor/tutorial/statichttpserver/StaticHttpServerTest.java
+++ officefloor/trunk/tutorials/StaticHttpServer/src/test/java/net/officefloor/tutorial/statichttpserver/StaticHttpServerTest.java
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package net.officefloor.example.statichttpserver;
+package net.officefloor.tutorial.statichttpserver;
 
 import junit.framework.TestCase;
 import net.officefloor.plugin.autowire.AutoWireAdministration;
