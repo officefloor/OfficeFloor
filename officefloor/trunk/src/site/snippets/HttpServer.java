22921c331032ee6e695ac01f4a3f664c70a496f4
Daniel <sagenschneider@users.noreply.github.com>
2011-04-26 11:39:20 +0000

    Fixing landing page for OfficeFloor.

--- officefloor/trunk/src/site/snippets/HttpServer.java
+++ officefloor/trunk/src/site/snippets/HttpServer.java
@@ -1,14 +1,15 @@
-// OfficeFloor source implementation to simplify creating a HTTP Server
+// OfficeFloor source to create an inline HTTP Server (useful for testing a single web page).
+// Note that applications would be configured using the WoOF graphical configuration.
 HttpServerAutoWireOfficeFloorSource server = new HttpServerAutoWireOfficeFloorSource();
-        
-// Add dynamic web page
-server.addHttpTemplate("example.ofp", Example.class, "example");
-        
+
+// Add a dynamic web page (ExampleBean provides the page logic and is just a POJO)
+server.addHttpTemplate("example.html", ExampleBean.class);
+
 // Add configured DataSource for dependency injection
 server.addManagedObject(DataSourceManagedObjectSource.class, null, DataSource.class).loadProperties("datasource.properties");
-        
-// Assign Team (specific thread pool) responsible for executing tasks with a DataSource dependency
+
+// Assign Team (specific thread pool) responsible for executing the methods with a DataSource dependency
 server.assignTeam(LeaderFollowerTeamSource.class, DataSource.class).addProperty("size", "10");
-        
+
 // Start the HTTP Server
 server.openOfficeFloor();
