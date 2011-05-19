38a871d3d9498de10fcfe61fd5f2d81dba657cb0
Daniel <sagenschneider@users.noreply.github.com>
2011-05-19 00:41:56 +0000

    Incomplete - task 3303844: ModuleRepository to read and create GWT modules - https://sourceforge.net/tracker/?func=detail&group_id=156349&atid=7994293303844
    Adding fix for compile to not break due to GWT version issue.

---
+++ officefloor/trunk/plugins/officeplugin_gwt/src/test/java/net/officefloor/plugin/gwt/module/GwtModuleRepositoryTest.java
@@ -0,0 +1,141 @@
+/*
+ * OfficeFloor - http://www.officefloor.net
+ * Copyright (C) 2005-2011 Daniel Sagenschneider
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.officefloor.plugin.gwt.module;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+
+import net.officefloor.frame.test.OfficeFrameTestCase;
+import net.officefloor.model.impl.repository.classloader.ClassLoaderConfigurationContext;
+import net.officefloor.model.impl.repository.memory.MemoryConfigurationItem;
+import net.officefloor.model.repository.ConfigurationItem;
+
+/**
+ * Tests the {@link GwtModuleRepository}.
+ * 
+ * @author Daniel Sagenschneider
+ */
+public class GwtModuleRepositoryTest extends OfficeFrameTestCase {
+
+	/**
+	 * {@link GwtModuleRepository} to test.
+	 */
+	private final GwtModuleRepository repository = new GwtModuleRepositoryImpl();
+
+	/**
+	 * Ensure able to retrieve the {@link GwtModuleModel}.
+	 */
+	public void testRetrieveGwtModule() throws Exception {
+
+		// Determine configuration item location
+		final String LOCATION = this.getFileLocation(this.getClass(),
+				"test.gwt.xml");
+
+		// Obtain the configuration item
+		ClassLoaderConfigurationContext context = new ClassLoaderConfigurationContext(
+				Thread.currentThread().getContextClassLoader());
+		ConfigurationItem configuration = context
+				.getConfigurationItem(LOCATION);
+		assertNotNull("Must have configuration", configuration);
+
+		// Retrieve the GWT Module
+		GwtModuleModel module = this.repository
+				.retrieveGwtModule(configuration);
+		assertNotNull("Should have module", module);
+
+		// Ensure correct values
+		assertEquals("Incorrect rename-to", "example", module.getRenameTo());
+		assertEquals("Incorrect EntryPoint class",
+				"net.officefloor.plugin.gwt.client.Example",
+				module.getEntryPointClassName());
+	}
+
+	/**
+	 * Ensure able to create the {@link GwtModuleModel}.
+	 */
+	public void testCreateGwtModule() throws Exception {
+
+		// Create the configuration item
+		ConfigurationItem configuration = new MemoryConfigurationItem("TEST");
+
+		// Configure the GWT Module
+		GwtModuleModel module = new GwtModuleModel();
+		module.setRenameTo("example");
+		module.setEntryPointClassName("net.officefloor.plugin.gwt.client.Example");
+
+		// Create the GWT Module
+		this.repository.createGwtModule(module, configuration);
+
+		// Obtain the file content
+		String actual = this.getText(configuration.getConfiguration());
+
+		// Validate content as expected
+		String expected = this.getText(this.findInputStream(this.getClass(),
+				"expected.gwt.xml"));
+		assertEquals("Incorrect created module", expected, actual);
+	}
+
+	/**
+	 * Ensure able to update the {@link GwtModuleModel}.
+	 */
+	public void testUpdateGwtModule() throws Exception {
+
+		// Create the configuration item
+		InputStream initial = this.findInputStream(this.getClass(),
+				"test.gwt.xml");
+		ConfigurationItem configuration = new MemoryConfigurationItem("TEST");
+		configuration.setConfiguration(initial);
+
+		// Configure to update the GWT Module
+		GwtModuleModel module = new GwtModuleModel();
+		module.setRenameTo("updated");
+		module.setEntryPointClassName("net.officefloor.plugin.gwt.client.Updated");
+
+		// Update the GWT Module
+		this.repository.updateGwtModule(module, configuration);
+
+		// Obtain the file content
+		String actual = this.getText(configuration.getConfiguration());
+
+		// Validate content as expected
+		String expected = this.getText(this.findInputStream(this.getClass(),
+				"updated.gwt.xml"));
+		assertEquals("Incorrect created module", expected, actual);
+	}
+
+	/**
+	 * Obtains the text.
+	 * 
+	 * @param content
+	 *            Content.
+	 * @return Text.
+	 */
+	private String getText(InputStream content) throws IOException {
+		Reader reader = new InputStreamReader(content);
+		StringWriter writer = new StringWriter();
+		for (int character = reader.read(); character != -1; character = reader
+				.read()) {
+			writer.write(character);
+		}
+		return writer.toString();
+	}
+
+}
