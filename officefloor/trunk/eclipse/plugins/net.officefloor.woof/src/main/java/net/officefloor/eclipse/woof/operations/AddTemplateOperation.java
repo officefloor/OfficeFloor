07737b5dc096e401c171c4aaa72097a490a95e90
sagenschneider <sagenschneider@ee502a36-3d87-488a-bc83-848c733a9ae9>
2011-05-27 14:02:57 +0000

    Complete - task 3304936: Add Template to configure GWT from WoOF Editor - https://sourceforge.net/tracker/?func=detail&group_id=156349&atid=7994293304936

--- officefloor/trunk/eclipse/plugins/net.officefloor.woof/src/main/java/net/officefloor/eclipse/woof/operations/AddTemplateOperation.java
+++ officefloor/trunk/eclipse/plugins/net.officefloor.woof/src/main/java/net/officefloor/eclipse/woof/operations/AddTemplateOperation.java
@@ -19,12 +19,22 @@ package net.officefloor.eclipse.woof.operations;
 
 import net.officefloor.compile.section.SectionType;
 import net.officefloor.eclipse.common.action.Operation;
+import net.officefloor.eclipse.common.editor.AbstractOfficeFloorEditor;
+import net.officefloor.eclipse.repository.project.ProjectConfigurationContext;
+import net.officefloor.eclipse.util.EclipseUtil;
 import net.officefloor.eclipse.wizard.template.HttpTemplateInstance;
 import net.officefloor.eclipse.wizard.template.HttpTemplateWizard;
 import net.officefloor.eclipse.woof.editparts.WoofEditPart;
 import net.officefloor.model.change.Change;
+import net.officefloor.model.gwt.module.GwtModuleModel;
+import net.officefloor.model.impl.repository.ModelRepositoryImpl;
+import net.officefloor.model.repository.ConfigurationContext;
 import net.officefloor.model.woof.WoofChanges;
 import net.officefloor.model.woof.WoofTemplateModel;
+import net.officefloor.plugin.gwt.module.GwtChanges;
+import net.officefloor.plugin.gwt.module.GwtChangesImpl;
+import net.officefloor.plugin.gwt.module.GwtFailureListener;
+import net.officefloor.plugin.gwt.module.GwtModuleRepositoryImpl;
 
 /**
  * {@link Operation} to add a {@link WoofTemplateModel}.
@@ -35,13 +45,22 @@ public class AddTemplateOperation extends
 		AbstractWoofChangeOperation<WoofEditPart> {
 
 	/**
+	 * {@link AbstractOfficeFloorEditor}.
+	 */
+	private final AbstractOfficeFloorEditor<?, ?> editor;
+
+	/**
 	 * Initiate.
 	 * 
 	 * @param woofChanges
 	 *            {@link WoofChanges}.
+	 * @param editor
+	 *            {@link AbstractOfficeFloorEditor}.
 	 */
-	public AddTemplateOperation(WoofChanges woofChanges) {
+	public AddTemplateOperation(WoofChanges woofChanges,
+			AbstractOfficeFloorEditor<?, ?> editor) {
 		super("Add template", WoofEditPart.class, woofChanges);
+		this.editor = editor;
 	}
 
 	/*
@@ -70,6 +89,39 @@ public class AddTemplateOperation extends
 
 		// Position template
 		context.positionModel(change.getTarget());
+
+		// Obtain the GWT details
+		String entryPointClassName = instance.getGwtEntryPointClassName();
+		if (!(EclipseUtil.isBlank(entryPointClassName))) {
+
+			// Create change to update GWT Module configuration
+			ClassLoader classLoader = Thread.currentThread()
+					.getContextClassLoader();
+			ConfigurationContext configurationContext = new ProjectConfigurationContext(
+					this.editor.getEditorInput());
+			GwtFailureListener listener = new GwtFailureListener() {
+				@Override
+				public void notifyFailure(String message, Throwable cause) {
+					// Provide error message of GWT failure
+					AddTemplateOperation.this.editor.messageError(message,
+							cause);
+				}
+			};
+			GwtChanges gwtChanges = new GwtChangesImpl(
+					new GwtModuleRepositoryImpl(new ModelRepositoryImpl(),
+							classLoader, "src/main/resources"),
+					configurationContext, listener);
+
+			// Create change to add the GWT Module
+			GwtModuleModel gwtModule = new GwtModuleModel(uri,
+					entryPointClassName);
+			Change<GwtModuleModel> gwtChange = gwtChanges.updateGwtModule(
+					gwtModule, null);
+
+			// Aggregate the changes
+			change = new AggregateChange<WoofTemplateModel>(change.getTarget(),
+					change.getChangeDescription(), change, gwtChange);
+		}
 
 		// Return the change to add the template
 		return change;
