e78093a65a6bcc5404fd90ed3b23c577fb0ec6a4
sagenschneider <sagenschneider@ee502a36-3d87-488a-bc83-848c733a9ae9>
2012-01-24 12:54:31 +0000

    Complete - task 3477688: Combine plug-ins into single Eclipse Plugin - https://sourceforge.net/tracker/?func=detail&group_id=156349&atid=7994293477688
    Also fixed definition of Input Managed Object (includes dependency configuration for input into private Managed Object Source tasks).

--- officefloor/trunk/core/officecompiler/src/test/java/net/officefloor/compile/impl/managedobject/CheckInputManagedObjectTest.java
+++ officefloor/trunk/core/officecompiler/src/test/java/net/officefloor/compile/impl/managedobject/CheckInputManagedObjectTest.java
@@ -18,10 +18,15 @@
 package net.officefloor.compile.impl.managedobject;
 
 import net.officefloor.compile.OfficeFloorCompiler;
+import net.officefloor.compile.managedobject.ManagedObjectDependencyType;
 import net.officefloor.compile.managedobject.ManagedObjectFlowType;
 import net.officefloor.compile.managedobject.ManagedObjectLoader;
+import net.officefloor.compile.managedobject.ManagedObjectTeamType;
 import net.officefloor.compile.managedobject.ManagedObjectType;
 import net.officefloor.compile.spi.officefloor.OfficeFloorInputManagedObject;
+import net.officefloor.frame.api.execute.Task;
+import net.officefloor.frame.spi.managedobject.ManagedObject;
+import net.officefloor.frame.spi.managedobject.source.ManagedObjectSource;
 import net.officefloor.frame.test.OfficeFrameTestCase;
 
 /**
@@ -65,6 +70,9 @@ public class CheckInputManagedObjectTest extends OfficeFrameTestCase {
 		this.recordReturn(this.managedObjectType,
 				this.managedObjectType.getFlowTypes(),
 				new ManagedObjectFlowType<?>[0]);
+		this.recordReturn(this.managedObjectType,
+				this.managedObjectType.getTeamTypes(),
+				new ManagedObjectTeamType[0]);
 
 		// Test
 		this.replayMockObjects();
@@ -77,9 +85,11 @@ public class CheckInputManagedObjectTest extends OfficeFrameTestCase {
 	}
 
 	/**
-	 * Validates that determine is {@link OfficeFloorInputManagedObject}.
+	 * Validates that determine is {@link OfficeFloorInputManagedObject} as has
+	 * {@link ManagedObjectFlowType} instances requiring the
+	 * {@link ManagedObject} binding to possibly be shared.
 	 */
-	public void testInputManagedObject() {
+	public void testInputManagedObjectByFlows() {
 
 		// Record checking if input managed object
 		this.recordReturn(this.managedObjectType,
@@ -94,6 +104,40 @@ public class CheckInputManagedObjectTest extends OfficeFrameTestCase {
 
 		// Verify that input
 		assertTrue("Should NOT be input managed object", isInput);
+	}
+
+	/**
+	 * Validates that determine is {@link OfficeFloorInputManagedObject} as
+	 * {@link ManagedObjectDependencyType} instances that need configuring as it
+	 * has private {@link Task} instances for the {@link ManagedObjectSource}
+	 * due to having {@link ManagedObjectTeamType} instances.
+	 */
+	public void testInputManagedObjectByTeams() {
+
+		final ManagedObjectTeamType teamType = this
+				.createMock(ManagedObjectTeamType.class);
+		final ManagedObjectDependencyType<?> dependencyType = this
+				.createMock(ManagedObjectDependencyType.class);
+
+		// Record checking if input managed object
+		this.recordReturn(this.managedObjectType,
+				this.managedObjectType.getFlowTypes(),
+				new ManagedObjectFlowType<?>[0]);
+		this.recordReturn(this.managedObjectType,
+				this.managedObjectType.getTeamTypes(),
+				new ManagedObjectTeamType[] { teamType });
+		this.recordReturn(this.managedObjectType,
+				this.managedObjectType.getDependencyTypes(),
+				new ManagedObjectDependencyType<?>[] { dependencyType });
+
+		// Test
+		this.replayMockObjects();
+		boolean isInput = this.loader
+				.isInputManagedObject(this.managedObjectType);
+		this.verifyMockObjects();
+
+		// Verify that input
+		assertTrue("Should be an input managed object", isInput);
 	}
 
 }
