72cc741433e683823bc73def02f2bbb2a54c3607
sagenschneider <sagenschneider@ee502a36-3d87-488a-bc83-848c733a9ae9>
2012-02-14 03:57:04 +0000

    Updating license and moving to version 2.1.0

--- officefloor/trunk/eclipse/plugins/net.officefloor.ui/src/main/java/net/officefloor/eclipse/extension/officesource/office/OfficeModelOfficeSourceExtension.java
+++ officefloor/trunk/eclipse/plugins/net.officefloor.ui/src/main/java/net/officefloor/eclipse/extension/officesource/office/OfficeModelOfficeSourceExtension.java
@@ -1,6 +1,6 @@
 /*
  * OfficeFloor - http://www.officefloor.net
- * Copyright (C) 2005-2011 Daniel Sagenschneider
+ * Copyright (C) 2005-2012 Daniel Sagenschneider
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
