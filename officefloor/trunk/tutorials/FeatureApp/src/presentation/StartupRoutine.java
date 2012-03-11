a940fb80888af78bbf1eecc1681423c7ffb0decd
Daniel <sagenschneider@users.noreply.github.com>
2012-03-11 13:07:33 +0000

    Providing configuration screen shot for the landing page.

---
+++ officefloor/trunk/tutorials/FeatureApp/src/presentation/StartupRoutine.java
@@ -0,0 +1,11 @@
+/**
+ * This is not used within the application. It is only here for the
+ * <code>presentation.woof</code> to give an overview screen shot of the
+ * graphical configuration.
+ */
+public class StartupRoutine {
+
+	public void startup() {
+	}
+
+}
