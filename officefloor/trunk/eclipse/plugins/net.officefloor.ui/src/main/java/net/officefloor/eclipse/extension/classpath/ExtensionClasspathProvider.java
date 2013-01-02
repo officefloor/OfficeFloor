8f4b6cdfebe8fd48c93a478edb3f621cbba9e84c
sagenschneider <sagenschneider@ee502a36-3d87-488a-bc83-848c733a9ae9>
2013-01-02 14:45:13 +0000

    Updating header to reflect year 2013

--- officefloor/trunk/eclipse/plugins/net.officefloor.ui/src/main/java/net/officefloor/eclipse/extension/classpath/ExtensionClasspathProvider.java
+++ officefloor/trunk/eclipse/plugins/net.officefloor.ui/src/main/java/net/officefloor/eclipse/extension/classpath/ExtensionClasspathProvider.java
@@ -1,6 +1,6 @@
 /*
  * OfficeFloor - http://www.officefloor.net
- * Copyright (C) 2005-2012 Daniel Sagenschneider
+ * Copyright (C) 2005-2013 Daniel Sagenschneider
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package net.officefloor.eclipse.extension.classpath;
 
 import net.officefloor.eclipse.OfficeFloorPlugin;
