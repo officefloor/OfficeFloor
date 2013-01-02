a95e12b867af06f2e370252c1238caf6309438cd
sagenschneider <sagenschneider@ee502a36-3d87-488a-bc83-848c733a9ae9>
2013-01-02 12:23:54 +0000

    Release 2.7.0

---
+++ officefloor/tags/release-2.7.0/trunk/maven/woof-archetype/src/test/resources/projects/woof/reference/src/main/java/net/officefloor/test/ria/client/CounterService.java
@@ -0,0 +1,11 @@
+package net.officefloor.test.ria.client;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+@RemoteServiceRelativePath("counter")
+public interface CounterService extends RemoteService {
+
+	public Integer updateCount(Integer value);
+
+}
