d482082a8a707e06e1c20adfa0d9629232986712
Daniel <sagenschneider@users.noreply.github.com>
2014-04-01 00:25:35 +0000

    Fixing unit tests for the latest versions.  Also tidying up woof-archetype to make version management of dependencies easier to change.

--- officefloor/trunk/plugins/officeplugin_jndi/src/test/java/net/officefloor/plugin/jndi/servlet/ServletIntegrationTest.java
+++ officefloor/trunk/plugins/officeplugin_jndi/src/test/java/net/officefloor/plugin/jndi/servlet/ServletIntegrationTest.java
@@ -19,9 +19,13 @@ package net.officefloor.plugin.jndi.servlet;
 
 import junit.framework.TestCase;
 import net.officefloor.frame.api.manage.OfficeFloor;
+import net.officefloor.plugin.socket.server.http.HttpTestUtil;
 
-import org.mortbay.jetty.testing.HttpTester;
-import org.mortbay.jetty.testing.ServletTester;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 /**
  * Ensure that {@link OfficeFloor} can be integrated into a Servlet.
@@ -33,20 +37,43 @@ public class ServletIntegrationTest extends TestCase {
 	/**
 	 * {@link ServletTester} to aid in testing.
 	 */
-	private final ServletTester tester = new ServletTester();
+	// private final ServletTester tester = new ServletTester();
+
+	/**
+	 * Port of {@link Server}.
+	 */
+	private final int port = HttpTestUtil.getAvailablePort();
+
+	/**
+	 * {@link Server}.
+	 */
+	private Server server;
 
 	@Override
 	protected void setUp() throws Exception {
+
+		// Start servlet container with servlet
+		this.server = new Server(this.port);
+		WebAppContext context = new WebAppContext();
+		// context.setBaseResource(Resource.newClassPathResource(this
+		// .getClass().getPackage().getName().replace('.', '/')
+		// + "/integrate"));
+		context.setContextPath("/");
+		// context.setSessionHandler(new SessionHandler());
+		context.addServlet(MockServlet.class, "/");
+		this.server.setHandler(context);
+
 		// Create the servlet tester
-		this.tester.setContextPath("/");
-		this.tester.addServlet(MockServlet.class, "/");
-		this.tester.start();
+		// this.tester.setContextPath("/");
+		// this.tester.addServlet(MockServlet.class, "/");
+		// this.tester.start();
 	}
 
 	@Override
 	protected void tearDown() throws Exception {
 		// Stop the servlet tester
-		this.tester.stop();
+		// this.tester.stop();
+		this.server.stop();
 	}
 
 	/**
@@ -55,24 +82,32 @@ public class ServletIntegrationTest extends TestCase {
 	 */
 	public void testOfficeFloorWithinServlet() throws Exception {
 
-		// Send request
-		HttpTester request = new HttpTester();
-		request.setMethod("GET");
-		request.addHeader("Host", "tester");
-		request.setVersion("HTTP/1.0");
-		request.setURI("/");
+		try (CloseableHttpClient client = HttpTestUtil.createHttpClient()) {
 
-		// Obtain response
-		HttpTester response = new HttpTester();
-		response.parse(this.tester.getResponses(request.generate()));
+			// Send request
+//			HttpTester request = new HttpTester();
+//			request.setMethod("GET");
+//			request.addHeader("Host", "tester");
+//			request.setVersion("HTTP/1.0");
+//			request.setURI("/");
 
-		// Ensure successful response
-		assertEquals("Unsuccessful request", 200, response.getStatus());
+			// Send request
+			HttpResponse response = client.execute(new HttpGet(
+					"http://localhost:" + this.port));
 
-		// Ensure OfficeFloor task invoked
-		String responseContent = response.getContent();		
-		boolean isTaskInvoked = Boolean.parseBoolean(responseContent.trim());
-		assertTrue("Task should be invoked", isTaskInvoked);
+			// Obtain response
+//			HttpTester response = new HttpTester();
+//			response.parse(this.tester.getResponses(request.generate()));
+
+			// Ensure successful response
+			assertEquals("Unsuccessful request", 200, response.getStatusLine().getStatusCode());
+
+			// Ensure OfficeFloor task invoked
+			String responseContent = HttpTestUtil.getEntityBody(response);
+			boolean isTaskInvoked = Boolean
+					.parseBoolean(responseContent.trim());
+			assertTrue("Task should be invoked", isTaskInvoked);
+		}
 	}
 
 }
