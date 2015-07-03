5d1f03ec5c5213106488a8012a535c1301153890
Daniel <sagenschneider@users.noreply.github.com>
2015-07-03 17:16:38 +0000

    Initial work to clean up managed objects sequentially.

---
+++ officefloor/trunk/core/officeframe/src/main/java/net/officefloor/frame/internal/structure/CleanupSequence.java
@@ -0,0 +1,48 @@
+/*
+ * OfficeFloor - http://www.officefloor.net
+ * Copyright (C) 2005-2013 Daniel Sagenschneider
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.officefloor.frame.internal.structure;
+
+/**
+ * Sequence of {@link JobNode} instances to undertake clean-up.
+ *
+ * @author Daniel Sagenschneider
+ */
+public interface CleanupSequence {
+
+	/**
+	 * <p>
+	 * Registers the {@link JobNode} that undertakes clean-up.
+	 * <p>
+	 * {@link JobNode} instances are executed in the order they are registered.
+	 * <p>
+	 * 
+	 * @param cleanupJob
+	 *            Clean up {@link JobNode}. Each {@link JobNode} is to be
+	 *            created within its own {@link ProcessState}. This is necessary
+	 *            as {@link ProcessCompletionListener} are registered to
+	 *            undertake the next clean up {@link JobNode}. Note:
+	 *            {@link JobNode} instances may invoke other {@link JobNode}
+	 *            instances for clean up, so the whole {@link ProcessState} must
+	 *            complete before the next {@link JobNode} may be started
+	 *            (typically because there may be dependencies between
+	 *            {@link JobNode} instances that must be respected in sequential
+	 *            clean up).
+	 */
+	void registerCleanUpJob(JobNode cleanupJob);
+
+}
