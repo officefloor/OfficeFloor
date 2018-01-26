efca79d2fab376243aedddc7f1af153c0b1ee989
Daniel Sagenschneider <daniel.sagenschneider@projix.com.au>
2018-01-27 03:20:08 +0800

    Stress testing pre-load administration of managed objects

--- officefloor/core/officeframe/src/test/stress/net/officefloor/frame/stress/administration/PreLoadAdministerStressTest.java
+++ officefloor/core/officeframe/src/test/stress/net/officefloor/frame/stress/administration/PreLoadAdministerStressTest.java
@@ -19,7 +19,15 @@ package net.officefloor.frame.stress.administration;
 
 import junit.framework.TestSuite;
 import net.officefloor.frame.api.administration.Administration;
+import net.officefloor.frame.api.administration.AdministrationContext;
+import net.officefloor.frame.api.administration.AdministrationFactory;
+import net.officefloor.frame.api.build.AdministrationBuilder;
+import net.officefloor.frame.api.build.None;
+import net.officefloor.frame.api.managedobject.ManagedObject;
+import net.officefloor.frame.internal.structure.ManagedObjectScope;
 import net.officefloor.frame.stress.AbstractStressTestCase;
+import net.officefloor.frame.test.ReflectiveFlow;
+import net.officefloor.frame.test.ReflectiveFunctionBuilder;
 
 /**
  * Stress test pre-load {@link Administration}.
@@ -34,8 +42,76 @@ public class PreLoadAdministerStressTest extends AbstractStressTestCase {
 
 	@Override
 	protected void constructTest(StressContext context) throws Exception {
-		// TODO Auto-generated method stub
-		
+
+		// Construct the managed object
+		this.constructManagedObject(new AdministeredManagedObject(), "MO", this.getOfficeName());
+
+		// Construct the functions
+		TestWork work = new TestWork(context);
+		ReflectiveFunctionBuilder task = this.constructFunction(work, "task");
+		context.loadOtherTeam(task.getBuilder());
+		AdministrationBuilder<None, None> admin = task.buildObject("MO", ManagedObjectScope.FUNCTION)
+				.preLoadAdminister("ADMIN", Object.class, work);
+		context.loadResponsibleTeam(admin);
+		task.setNextFunction("next");
+		ReflectiveFunctionBuilder next = this.constructFunction(work, "next");
+		context.loadOtherTeam(next.getBuilder());
+		next.buildFlow("task", null, false);
+
+		// Test
+		context.setInitialFunction("task", null);
+	}
+
+	/**
+	 * Test functionality.
+	 */
+	public class TestWork implements AdministrationFactory<Object, None, None>, Administration<Object, None, None> {
+
+		private final StressContext context;
+
+		private boolean isPreLoadAdministered = false;
+
+		public TestWork(StressContext context) {
+			this.context = context;
+		}
+
+		public void task(AdministeredManagedObject object) {
+			assertTrue("Should pre-load administer managed object", this.isPreLoadAdministered);
+		}
+
+		public void next(ReflectiveFlow task) {
+
+			// Reset to ensure administer again
+			this.isPreLoadAdministered = false;
+
+			// Determine if continue
+			if (this.context.incrementIterationAndIsComplete()) {
+				return;
+			}
+
+			// Repeat
+			task.doFlow(null, null);
+		}
+
+		/*
+		 * ==================== Administration ====================
+		 */
+
+		@Override
+		public Administration<Object, None, None> createAdministration() throws Throwable {
+			return this;
+		}
+
+		@Override
+		public void administer(AdministrationContext<Object, None, None> context) throws Throwable {
+			this.isPreLoadAdministered = true;
+		}
+	}
+
+	/**
+	 * {@link ManagedObject}.
+	 */
+	public class AdministeredManagedObject {
 	}
 
 }
