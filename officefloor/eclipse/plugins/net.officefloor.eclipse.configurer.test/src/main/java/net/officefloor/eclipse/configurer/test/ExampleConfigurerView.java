ae6e16a6734728de66faea089951d86b8b42f94c
Daniel Sagenschneider <daniel.sagenschneider@projix.com.au>
2018-04-07 18:31:58 +0800

    Renaming to net.officefloor.eclipse.configurer
    Also starting test project for Workspace bound functionality (e.g. Class
    look ups)

---
+++ officefloor/eclipse/plugins/net.officefloor.eclipse.configurer.test/src/main/java/net/officefloor/eclipse/configurer/test/ExampleConfigurerView.java
@@ -0,0 +1,50 @@
+/*
+ * OfficeFloor - http://www.officefloor.net
+ * Copyright (C) 2005-2018 Daniel Sagenschneider
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.officefloor.eclipse.configurer.test;
+
+import javax.inject.Inject;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+
+/**
+ * Example configurer view.
+ * 
+ * @author Daniel Sagenschneider
+ */
+public class ExampleConfigurerView {
+
+	@Inject
+	public ExampleConfigurerView(Composite parent) throws Exception {
+
+		// Obtain the java project
+		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+		IProject project = root.getProject("Test");
+		IJavaProject javaProject = JavaCore.create(project);
+
+		Label label = new Label(parent, SWT.NONE);
+		label.setText("Project " + javaProject.getRawClasspath());
+
+	}
+}
