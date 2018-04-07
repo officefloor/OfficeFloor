32f24962e1ac09116f2195c83f477890ae53ec5e
Daniel Sagenschneider <daniel.sagenschneider@projix.com.au>
2018-04-08 00:33:24 +0800

    Refactored to Editor and Configurer
    Also, moved test classes to own test plugins

--- officefloor/eclipse/plugins/net.officefloor.eclipse.editor.test/src/main/java/net/officefloor/eclipse/editor/test/ExampleEditorMain.java
+++ officefloor/eclipse/plugins/net.officefloor.eclipse.editor.test/src/main/java/net/officefloor/eclipse/editor/test/ExampleEditorMain.java
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package net.officefloor.eclipse.example;
+package net.officefloor.eclipse.editor.test;
 
 import javafx.application.Application;
 import javafx.scene.layout.HBox;
