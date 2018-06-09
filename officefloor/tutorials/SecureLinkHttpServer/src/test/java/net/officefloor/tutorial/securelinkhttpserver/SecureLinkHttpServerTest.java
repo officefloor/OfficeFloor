c92e94634b4ca48c898ab1c2e668355e78e4a211
Daniel Sagenschneider <daniel.sagenschneider@projix.com.au>
2018-06-10 01:06:29 +0800

    Tutorial tidy up

--- officefloor/tutorials/SecureLinkHttpServer/src/test/java/net/officefloor/tutorial/securelinkhttpserver/SecureLinkHttpServerTest.java
+++ officefloor/tutorials/SecureLinkHttpServer/src/test/java/net/officefloor/tutorial/securelinkhttpserver/SecureLinkHttpServerTest.java
@@ -17,59 +17,48 @@
  */
 package net.officefloor.tutorial.securelinkhttpserver;
 
+import static org.junit.Assert.assertTrue;
+
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.util.EntityUtils;
+import org.junit.Rule;
+import org.junit.Test;
 
-import junit.framework.TestCase;
 import net.officefloor.OfficeFloorMain;
-import net.officefloor.server.http.HttpClientTestUtil;
-import net.officefloor.server.http.impl.HttpServerLocationImpl;
+import net.officefloor.server.http.HttpClientRule;
+import net.officefloor.test.OfficeFloorRule;
 
 /**
  * Tests the Secure Link.
  * 
  * @author Daniel Sagenschneider
  */
-public class SecureLinkHttpServerTest extends TestCase {
+public class SecureLinkHttpServerTest {
 
 	/**
-	 * {@link CloseableHttpClient}.
+	 * Run application.
 	 */
-	private final CloseableHttpClient client = HttpClientTestUtil.createHttpClient();
-
-	@Override
-	protected void tearDown() throws Exception {
-		// Ensure stop
-		try {
-			this.client.close();
-		} finally {
-			OfficeFloorMain.close();
-		}
+	public static void main(String[] args) throws Exception {
+		OfficeFloorMain.main(args);
 	}
 
-	/**
-	 * Ensure the link is secure.
-	 */
 	// START SNIPPET: tutorial
-	public void testLinkRenderedSecure() throws Exception {
+	@Rule
+	public OfficeFloorRule officeFloor = new OfficeFloorRule();
 
-		// Start the server
-		OfficeFloorMain.open();
+	@Rule
+	public HttpClientRule client = new HttpClientRule();
 
-		// Obtain the host name
-		String hostName = HttpServerLocationImpl.getDefaultHostName();
+	@Test
+	public void ensureLinkRenderedSecure() throws Exception {
 
 		// Obtain the page
-		HttpResponse response = this.client.execute(new HttpGet("http://" + hostName + ":7878"));
+		HttpResponse response = this.client.execute(new HttpGet("http://localhost:7878"));
 		String renderedPage = EntityUtils.toString(response.getEntity());
 
-		System.out.println("page: " + renderedPage);
-
 		// Ensure login form (link) is secure
-		assertTrue("Login form should be secure",
-				renderedPage.contains("form action=\"https://" + hostName + ":7979/+login"));
+		assertTrue("Login form should be secure", renderedPage.contains("form action=\"https://localhost:7979/+login"));
 	}
 	// END SNIPPET: tutorial
 
