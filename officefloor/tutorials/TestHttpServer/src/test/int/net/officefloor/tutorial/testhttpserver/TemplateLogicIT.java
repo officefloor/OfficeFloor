7f9f4880fe6283b6ca8850e25c2c03a09327e6e3
Daniel Sagenschneider <daniel.sagenschneider@projix.com.au>
2018-06-12 22:30:20 +0800

    Including integration tests

--- officefloor/tutorials/TestHttpServer/src/test/int/net/officefloor/tutorial/testhttpserver/TemplateLogicIT.java
+++ officefloor/tutorials/TestHttpServer/src/test/int/net/officefloor/tutorial/testhttpserver/TemplateLogicIT.java
@@ -17,36 +17,37 @@
  */
 package net.officefloor.tutorial.testhttpserver;
 
+import static org.junit.Assert.assertTrue;
+
 import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.util.EntityUtils;
-import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
 
-import net.officefloor.server.http.HttpClientTestUtil;
+import net.officefloor.server.http.HttpClientRule;
 
 /**
  * Tests the {@link TemplateLogic}.
  * 
  * @author Daniel Sagenschneider
  */
-public class TemplateLogicIT extends Assert {
+public class TemplateLogicIT {
 
 	// START SNIPPET: integration
+	@Rule
+	public HttpClientRule client = new HttpClientRule();
+
 	@Test
 	public void integrationTest() throws Exception {
 
-		try (CloseableHttpClient client = HttpClientTestUtil.createHttpClient()) {
+		// Send request to add
+		HttpPost request = new HttpPost("http://localhost:7878/template+add?a=1&b=2");
+		HttpResponse response = client.execute(request);
 
-			// Send request to add
-			HttpGet request = new HttpGet("http://localhost:7878/template-add.woof?a=1&b=2");
-			HttpResponse response = client.execute(request);
-
-			// Ensure added the values
-			String entity = EntityUtils.toString(response.getEntity());
-			assertTrue("Should have added the values", entity.contains("= 3"));
-		}
+		// Ensure added the values
+		String entity = EntityUtils.toString(response.getEntity());
+		assertTrue("Should have added the values: " + entity, entity.contains("= 3"));
 	}
 	// END SNIPPET: integration
 
