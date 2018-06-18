15d4a84725d0d3cddee7a1103d1ef4a9cedb85a0
Daniel Sagenschneider <daniel.sagenschneider@projix.com.au>
2018-06-18 21:19:27 +0800

    Running comparisons
    Also, tidy up of documentation

---
+++ benchmarks/run_comparison.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -e
+set -x
+
+# Obtain the script directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
+# TODO REMOVE
+#if [ ! -f "${DIR}/run_comparison.sh" ]; then
+
+# Obtain latest OfficeFloor code
+if [ -d "${DIR}/OfficeFloorFrameworkBenchmarks" ]; then
+	rm -rf "${DIR}/OfficeFloorFrameworkBenchmarks"
+fi
+if [ ! -d "${DIR}/OfficeFloorFrameworkBenchmarks" ]; then
+	mkdir "${DIR}/OfficeFloorFrameworkBenchmarks"
+fi
+cd "${DIR}/OfficeFloorFrameworkBenchmarks"
+git clone --depth 1 git@github.com:sagenschneider/FrameworkBenchmarks.git
+
+# Obtain latest TechEmpower code
+if [ -d "${DIR}/TechEmpowerFrameworkBenchmarks" ]; then
+	rm -rf "${DIR}/TechEmpowerFrameworkBenchmarks"
+fi
+if [ ! -d "${DIR}/TechEmpowerFrameworkBenchmarks" ]; then
+	mkdir "${DIR}/TechEmpowerFrameworkBenchmarks"
+fi
+cd "${DIR}/TechEmpowerFrameworkBenchmarks"
+git clone --depth 1 git@github.com:TechEmpower/FrameworkBenchmarks.git
+
+#fi
+
+
+# Create run directory
+if [ ! -d "${DIR}/FrameworkBenchmarks" ]; then
+	mkdir "${DIR}/FrameworkBenchmarks"
+fi
+cp -Rf "${DIR}/TechEmpowerFrameworkBenchmarks/FrameworkBenchmarks" "${DIR}/FrameworkBenchmarks"
+if [ ! -d "${DIR}/FrameworkBenchmarks/FrameworkBenchmarks/frameworks/Java/OfficeFloor" ]; then
+	rm -rf "${DIR}/FrameworkBenchmarks/FrameworkBenchmarks/frameworks/Java/OfficeFloor"
+fi
+cp -Rf "${DIR}/OfficeFloorFrameworkBenchmarks/FrameworkBenchmarks/frameworks/Java/OfficeFloor" "${DIR}/FrameworkBenchmarks/FrameworkBenchmarks/frameworks/Java/OfficeFloor"
+
+# Run the comparison
+cd "${DIR}/FrameworkBenchmarks/FrameworkBenchmarks"
+./tfb --test h2o actix-raw rapidoid-http-fast vertx-postgres vertx-web-postgres
