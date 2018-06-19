df5ca7183811b2b3bc7c9b803f2dbb5834a9e4aa
Daniel Sagenschneider <daniel.sagenschneider@projix.com.au>
2018-06-19 17:28:38 +0800

    Graphical Configuration example

---
+++ officefloor/tutorials/GraphicalConfigurationExample/src/main/java/net/officefloor/tutorials/graphicalconfigurationexample/RestLogic.java
@@ -0,0 +1,19 @@
+package net.officefloor.tutorials.graphicalconfigurationexample;
+
+/**
+ * REST logic.
+ * 
+ * @author daniel
+ */
+public class RestLogic {
+	
+	public void service() {
+	}
+
+	public void getItem() {
+	}
+
+	public void postItem() {
+	}
+
+}
