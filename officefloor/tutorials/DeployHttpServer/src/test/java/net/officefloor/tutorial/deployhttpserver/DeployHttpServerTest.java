73aa9e83e47ad2dd971938cadcf844450ea4d7fd
Daniel Sagenschneider <daniel.sagenschneider@projix.com.au>
2018-06-28 18:14:42 +0800

    Adding deploy tutorial
    Also, providing editor icon for WoOF editor

---
+++ officefloor/tutorials/DeployHttpServer/src/test/java/net/officefloor/tutorial/deployhttpserver/DeployHttpServerTest.java
@@ -0,0 +1,54 @@
+/*
+ * OfficeFloor - http://www.officefloor.net
+ * Copyright (C) 2005-2018 Daniel Sagenschneider
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.officefloor.tutorial.deployhttpserver;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.util.EntityUtils;
+import org.junit.Rule;
+import org.junit.Test;
+
+import net.officefloor.server.http.HttpClientRule;
+import net.officefloor.test.OfficeFloorRule;
+
+/**
+ * Tests the deployed HTTP server.
+ * 
+ * @author Daniel Sagenschneider
+ */
+public class DeployHttpServerTest {
+
+	@Rule
+	public OfficeFloorRule officefloor = new OfficeFloorRule();
+
+	@Rule
+	public HttpClientRule client = new HttpClientRule();
+
+	@Test
+	public void ensureRunning() throws IOException {
+		HttpResponse response = this.client.execute(new HttpGet(this.client.url("/deploy")));
+		assertEquals("Should be succesful", 200, response.getStatusLine().getStatusCode());
+		assertEquals("Incorrect response", "<html><body>Deployed</body></html>",
+				EntityUtils.toString(response.getEntity()));
+	}
+
+}
