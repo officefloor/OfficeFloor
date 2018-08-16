2a150c89ed30fac0cdfa37592d072f23a4e54922
Daniel Sagenschneider <daniel.sagenschneider@projix.com.au>
2018-08-16 08:45:15 +0800

    officefloor-raw passing fortunes test

--- benchmarks/test/woof_raw_test/src/test/java/net/officefloor/benchmark/TestSuite.java
+++ benchmarks/test/woof_raw_test/src/test/java/net/officefloor/benchmark/TestSuite.java
@@ -27,7 +27,8 @@ import org.junit.runners.Suite.SuiteClasses;
  * Tests.
  */
 @RunWith(Suite.class)
-@SuiteClasses({ TestSuite.RawJsonTest.class, TestSuite.RawPlaintextTest.class, TestSuite.RawDbTest.class })
+@SuiteClasses({ TestSuite.RawJsonTest.class, TestSuite.RawPlaintextTest.class, TestSuite.RawDbTest.class,
+		TestSuite.RawFortunesTest.class })
 public class TestSuite {
 
 	public static void start() throws Exception {
@@ -63,6 +64,18 @@ public class TestSuite {
 	}
 
 	public static class RawDbTest extends DbTest {
+		@Before
+		public void start() throws Exception {
+			TestSuite.start();
+		}
+
+		@After
+		public void stop() throws Exception {
+			TestSuite.stop();
+		}
+	}
+
+	public static class RawFortunesTest extends FortunesTest {
 		@Before
 		public void start() throws Exception {
 			TestSuite.start();
