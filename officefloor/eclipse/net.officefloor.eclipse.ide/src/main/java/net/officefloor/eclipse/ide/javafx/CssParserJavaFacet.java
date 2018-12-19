f55c0e877cb10afe31aed240aca8ee6c4a69302e
Daniel Sagenschneider <daniel.sagenschneider@projix.com.au>
2018-12-19 18:05:15 +0800

    Committing to merge in latest of master

---
+++ officefloor/eclipse/net.officefloor.eclipse.ide/src/main/java/net/officefloor/eclipse/ide/javafx/CssParserJavaFacet.java
@@ -0,0 +1,12 @@
+package net.officefloor.eclipse.ide.javafx;
+
+import net.officefloor.frame.compatibility.JavaFacet;
+
+/**
+ * CssParser {@link JavaFacet}.
+ * 
+ * @author Daniel Sagenschneider
+ */
+public class CssParserJavaFacet {
+
+}
