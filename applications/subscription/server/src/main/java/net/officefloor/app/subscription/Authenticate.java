6c8011c2d203e3151b58be1a9da6a3430e020b09
Daniel Sagenschneider <daniel.sagenschneider@projix.com.au>
2018-12-27 23:38:32 +0800

    Able to authenticate with Google

--- applications/subscription/server/src/main/java/net/officefloor/app/subscription/Authenticate.java
+++ applications/subscription/server/src/main/java/net/officefloor/app/subscription/Authenticate.java
@@ -1,5 +1,15 @@
 package net.officefloor.app.subscription;
 
+import java.util.Collections;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+
 import lombok.Data;
 import net.officefloor.web.HttpObject;
 
@@ -8,14 +18,31 @@ import net.officefloor.web.HttpObject;
  */
 public class Authenticate {
 
+	private static HttpTransport transport = new NetHttpTransport();
+
+	private static JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+
 	@Data
 	@HttpObject
 	public static class Authentication {
 		String idToken;
 	}
 
-	public void service(Authentication authentication) {
-		System.out.println("Authentication: " + authentication.getIdToken());
+	public void service(Authentication authentication) throws Exception {
+
+		// Build verifier
+		GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(transport, jsonFactory)
+				.setAudience(Collections
+						.singletonList("443132781504-19vekci7r4t2qvqpbg9q1s32kjnp1c7t.apps.googleusercontent.com"))
+				.build();
+
+		// Verify token
+		GoogleIdToken token = verifier.verify(authentication.getIdToken());
+
+		// Provide details
+		Payload payload = token.getPayload();
+		System.out.println(
+				"User: " + payload.get("name") + " (" + payload.getSubject() + " - " + payload.getEmail() + ")");
 	}
 
 }
