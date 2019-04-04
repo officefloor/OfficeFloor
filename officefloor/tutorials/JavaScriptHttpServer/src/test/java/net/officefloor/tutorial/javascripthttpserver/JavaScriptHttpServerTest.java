7f49bccb3326529a5f91d05f5170652fa3979422
Daniel Sagenschneider <daniel@officefloor.net>
2019-04-04 17:40:55 +0800

    JavaScript tutorial code written

--- officefloor/tutorials/JavaScriptHttpServer/src/test/java/net/officefloor/tutorial/javascripthttpserver/JavaScriptHttpServerTest.java
+++ officefloor/tutorials/JavaScriptHttpServer/src/test/java/net/officefloor/tutorial/javascripthttpserver/JavaScriptHttpServerTest.java
@@ -43,20 +43,21 @@ public class JavaScriptHttpServerTest {
 		MockHttpResponse response = this.server
 				.send(MockHttpServer.mockRequest().header("Content-Type", "application/json")
 						.entity(mapper.writeValueAsString(new Request(-1, "Daniel"))));
-		response.assertResponse(400, "Invalid identifier");
+		response.assertResponse(400, "{\"error\":\"Invalid identifier\"}");
 	}
 
 	@Test
 	public void invalidName() throws Exception {
-		MockHttpResponse response = this.server
-				.send(MockHttpServer.mockRequest().entity(mapper.writeValueAsString(new Request(1, ""))));
-		response.assertResponse(400, "Must provide name");
+		MockHttpResponse response = this.server.send(MockHttpServer.mockRequest()
+				.header("Content-Type", "application/json").entity(mapper.writeValueAsString(new Request(1, ""))));
+		response.assertResponse(400, "{\"error\":\"Must provide name\"}");
 	}
 
 	@Test
 	public void validRequest() throws Exception {
 		MockHttpResponse response = this.server
-				.send(MockHttpServer.mockRequest().entity(mapper.writeValueAsString(new Request(1, ""))));
+				.send(MockHttpServer.mockRequest().header("Content-Type", "application/json")
+						.entity(mapper.writeValueAsString(new Request(1, "Daniel"))));
 		response.assertResponse(200, mapper.writeValueAsString(new Response("successful")));
 	}
 }
