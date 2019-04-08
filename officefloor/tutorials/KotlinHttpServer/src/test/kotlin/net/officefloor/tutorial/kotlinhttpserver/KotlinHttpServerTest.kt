dd96289fbad4bc1f0c7c1457230406337c259949
Daniel Sagenschneider <daniel@officefloor.net>
2019-04-08 13:42:43 +0800

    Kotlin tutorial written

--- officefloor/tutorials/KotlinHttpServer/src/test/kotlin/net/officefloor/tutorial/kotlinhttpserver/KotlinHttpServerTest.kt
+++ officefloor/tutorials/KotlinHttpServer/src/test/kotlin/net/officefloor/tutorial/kotlinhttpserver/KotlinHttpServerTest.kt
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.databind.ObjectMapper
 /**
  * Tests the Kotlin HTTP server.
  */
+// START SNIPPET: tutorial
 class KotlinHttpServerTest {
 
 	val mapper = ObjectMapper()
@@ -31,3 +32,4 @@ class KotlinHttpServerTest {
 		response.assertResponse(200, mapper.writeValueAsString(KotlinResponse("Hello Daniel from Kotlin")));
 	}
 }
+// END SNIPPET: tutorial
