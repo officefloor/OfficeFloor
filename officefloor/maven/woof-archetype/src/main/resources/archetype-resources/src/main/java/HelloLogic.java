07bfe3ce649300540a31809020946ae8e9721ac9
Daniel Sagenschneider <daniel@officefloor.net>
2019-11-11 22:47:46 +0800

    Tidied up My First Application tutorial

--- officefloor/maven/woof-archetype/src/main/resources/archetype-resources/src/main/java/HelloLogic.java
+++ officefloor/maven/woof-archetype/src/main/resources/archetype-resources/src/main/java/HelloLogic.java
@@ -1,27 +1,17 @@
 package ${package};
 
+import lombok.Value;
 import net.officefloor.web.HttpPathParameter;
 import net.officefloor.web.ObjectResponse;
 
-/**
- * Logic for hello world.
- */
 public class HelloLogic {
-
-	public static class Message {
-		private String message;
-
-		// Consider simplifying with Lombok
-		private Message(String message) {
-			this.message = message;
-		}
-
-		public String getMessage() {
-			return this.message;
-		}
-	}
 
 	public void hello(@HttpPathParameter("name") String name, ObjectResponse<Message> response) {
 		response.send(new Message("Hello " + name));
+	}
+
+	@Value
+	public static class Message {
+		private String message;
 	}
 }
