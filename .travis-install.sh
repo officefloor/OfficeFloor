0a6156aac50cfab12061725de03e1df4bcdda827
Daniel <sagenschneider@users.noreply.github.com>
2019-11-26 11:58:27 +0800

    Fixing for running scripts in current shell

--- .travis-install.sh
+++ .travis-install.sh
@@ -8,3 +8,6 @@ set -e
 # Default Travix install except avoids archetype integration test also
 cd officefloor/bom
 mvn clean install -DskipTests=true -DskipITs -Dmaven.javadoc.skip=true -Darchetype.test.skip=true -B -V -e
+
+# Return to starting directory
+cd ../..
