668146925fc6538064011424036c8da317bae956
Daniel Sagenschneider <daniel@officefloor.net>
2019-11-26 10:41:11 +0800

    Attempting to fix Travis build

--- .travis-install.sh
+++ .travis-install.sh
@@ -2,6 +2,9 @@
 set -x
 set -e
 
+# Ensure appropriate maven version installed
+./.travis-maven.sh
+
 # Default Travix install except avoids archetype integration test also
 cd officefloor/bom
 mvn clean install -DskipTests=true -DskipITs -Dmaven.javadoc.skip=true -Darchetype.test.skip=true -B -V -e
