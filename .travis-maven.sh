44ca78d36d87a1b96a926f9f6d4bc8683f9af070
Daniel Sagenschneider <daniel@officefloor.net>
2019-11-26 09:04:20 +0800

    Allowing customising of Maven version
    This is to allow fixes for Tycho incompatibilities

---
+++ .travis-maven.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -x
+set -e
+
+if [ -z "${CUSTOM_MVN_VERSION}" ]; then
+	# No customisation of maven
+	echo "Using default Maven"
+else
+	# Install custom version of maven if specified
+	echo "Download Maven ${CUSTOM_MVN_VERSION}....";
+	wget https://archive.apache.org/dist/maven/maven-3/${CUSTOM_MVN_VERSION}/binaries/apache-maven-${CUSTOM_MVN_VERSION}-bin.zip || travis_terminate 1;
+	unzip -qq apache-maven-${CUSTOM_MVN_VERSION}-bin.zip || travis_terminate 1;
+	export M2_HOME=$PWD/apache-maven-${CUSTOM_MVN_VERSION};
+	export PATH=$M2_HOME/bin:$PATH;
+fi
+mvn -version;
+
