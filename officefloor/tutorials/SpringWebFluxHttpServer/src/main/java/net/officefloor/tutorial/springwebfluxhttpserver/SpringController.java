8c3070b2e3b20fee4314e8579313177c95ec17b4
Daniel Sagenschneider <daniel@officefloor.net>
2020-04-29 02:45:03 +0800

    Fixed for Flux on Windows

--- officefloor/tutorials/SpringWebFluxHttpServer/src/main/java/net/officefloor/tutorial/springwebfluxhttpserver/SpringController.java
+++ officefloor/tutorials/SpringWebFluxHttpServer/src/main/java/net/officefloor/tutorial/springwebfluxhttpserver/SpringController.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 /**
@@ -18,7 +20,7 @@ public class SpringController {
 
 	@GetMapping("/html")
 	public Mono<String> html(@RequestParam String name, Model model) {
-		model.addAttribute("name", name);
+		model.addAttribute("names", new ReactiveDataDriverContextVariable(Flux.just(name), 1));
 		return Mono.just("simple");
 	}
 }
