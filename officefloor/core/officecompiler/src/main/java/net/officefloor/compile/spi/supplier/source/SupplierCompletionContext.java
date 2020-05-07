cb03e003d89918e36507035909c00b9d10c99829
Daniel Sagenschneider <daniel@officefloor.net>
2020-05-07 23:45:22 +0800

    Tidying up documentation

--- officefloor/core/officecompiler/src/main/java/net/officefloor/compile/spi/supplier/source/SupplierCompletionContext.java
+++ officefloor/core/officecompiler/src/main/java/net/officefloor/compile/spi/supplier/source/SupplierCompletionContext.java
@@ -1,5 +1,7 @@
 package net.officefloor.compile.spi.supplier.source;
 
+import net.officefloor.frame.api.managedobject.ManagedObject;
+
 /**
  * Completion context for the {@link SupplierSource}.
  * 
@@ -11,9 +13,10 @@ public interface SupplierCompletionContext extends SupplierCompileContext {
 	 * <p>
 	 * Obtains the {@link AvailableType} instances.
 	 * <p>
-	 * These are all the {@link AvailableType} instances at the time of calling this
-	 * method. {@link SupplierSource} instances may add further
-	 * {@link AvailableType} instances.
+	 * Note that {@link ManagedObject} instances provided by {@link SupplierSource}
+	 * instances are not included. This is because {@link SupplierSource} instances
+	 * are not completed at this time to list their available {@link ManagedObject}
+	 * instances.
 	 * 
 	 * @return {@link AvailableType} instances.
 	 */
