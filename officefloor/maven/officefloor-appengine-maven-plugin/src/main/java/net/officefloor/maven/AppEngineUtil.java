78ebec5bb817a7278f963019de37d3b1c39325e9
Daniel Sagenschneider <daniel@officefloor.net>
2020-12-03 00:37:14 +0800

    Cleaning up emulator after shut down

--- officefloor/maven/officefloor-appengine-maven-plugin/src/main/java/net/officefloor/maven/AppEngineUtil.java
+++ officefloor/maven/officefloor-appengine-maven-plugin/src/main/java/net/officefloor/maven/AppEngineUtil.java
@@ -1,6 +1,7 @@
 package net.officefloor.maven;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -10,9 +11,14 @@ import java.util.List;
 import java.util.Properties;
 import java.util.function.BiConsumer;
 
+import org.apache.http.NoHttpResponseException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.descriptor.PluginDescriptor;
+import org.apache.maven.plugin.logging.Log;
 
 import com.google.cloud.datastore.testing.LocalDatastoreHelper;
 import com.google.cloud.tools.maven.run.AbstractRunMojo;
@@ -88,16 +94,7 @@ public class AppEngineUtil {
 		}
 
 		// Obtain the appengine emulator enhancement
-		File officeServerAppEngineEmulatorJar = null;
-		for (Artifact artifact : plugin.getArtifacts()) {
-			if (("net.officefloor.server".equals(artifact.getGroupId()))
-					&& ("officeserver_appengineemulator".equals(artifact.getArtifactId()))) {
-				officeServerAppEngineEmulatorJar = artifact.getFile();
-			}
-		}
-		if (officeServerAppEngineEmulatorJar == null) {
-			throw new MojoExecutionException("Failed to obtain AppEngine enhancement jar");
-		}
+		File officeServerAppEngineEmulatorJar = getOfficeFloorAppEngineEmulatorJar(plugin);
 
 		// Determine if already available
 		File webLibDir = new File(new File(targetDir, finalName), "WEB-INF/lib");
@@ -113,4 +110,73 @@ public class AppEngineUtil {
 			}
 		}
 	}
+
+	/**
+	 * Tears down the App Engine.
+	 * 
+	 * @param targetDir Target directory.
+	 * @param finalName Final name.
+	 * @param plugin    {@link PluginDescriptor}.
+	 * @throws MojoExecutionException If fails to tear down App Engine.
+	 */
+	public static void tearDownAppEngine(File targetDir, String finalName, PluginDescriptor plugin, Log log)
+			throws MojoExecutionException {
+
+		// Obtain the location of datastore
+		String location;
+		try {
+			File appEnginePropertiesFile = getAppEnginePropertiesFile(targetDir);
+			Properties properties = new Properties();
+			properties.load(new FileReader(appEnginePropertiesFile));
+			location = properties.getProperty(DatastoreHelper.LOCAL_HOST_ENV_VAR);
+		} catch (IOException ex) {
+			throw new MojoExecutionException("Failed to obtain datastore location", ex);
+		}
+
+		// Stop the data store
+		log.info("Stopping datastore " + location);
+		try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
+			client.execute(new HttpPost("http://" + location + "/shutdown"));
+		} catch (NoHttpResponseException ex) {
+			// Ignore as may shutdown before sending response
+		} catch (IOException ex) {
+			throw new MojoExecutionException("Failed to shutdown datastore", ex);
+		}
+
+		// Remove the appengine emulator enhancement
+		File officeServerAppEngineEmulatorJar = getOfficeFloorAppEngineEmulatorJar(plugin);
+		File webLibDir = new File(new File(targetDir, finalName), "WEB-INF/lib");
+		File targetJarFile = new File(webLibDir, officeServerAppEngineEmulatorJar.getName());
+		if (targetJarFile.exists()) {
+			try {
+				Files.delete(targetJarFile.toPath());
+			} catch (IOException ex) {
+				throw new MojoExecutionException("Failed to clean AppEngine enhancements", ex);
+			}
+		}
+	}
+
+	/**
+	 * Obtains the {@link File} to the OfficeFloor AppEngine Emulator Jar.
+	 * 
+	 * @param plugin {@link PluginDescriptor}.
+	 * @return {@link File} to the OfficeFloor AppEngine Emulator Jar.
+	 * @throws MojoExecutionException
+	 */
+	private static File getOfficeFloorAppEngineEmulatorJar(PluginDescriptor plugin) throws MojoExecutionException {
+
+		// Obtain the appengine emulator enhancement
+		File officeServerAppEngineEmulatorJar = null;
+		for (Artifact artifact : plugin.getArtifacts()) {
+			if (("net.officefloor.server".equals(artifact.getGroupId()))
+					&& ("officeserver_appengineemulator".equals(artifact.getArtifactId()))) {
+				officeServerAppEngineEmulatorJar = artifact.getFile();
+			}
+		}
+		if (officeServerAppEngineEmulatorJar == null) {
+			throw new MojoExecutionException("Failed to obtain AppEngine enhancement jar");
+		}
+		return officeServerAppEngineEmulatorJar;
+	}
+
 }
