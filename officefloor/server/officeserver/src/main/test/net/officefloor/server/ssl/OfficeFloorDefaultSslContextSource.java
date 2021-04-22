88012b11157787cd5613b1c02c37d58dd1b2e8e7
Daniel Sagenschneider <daniel@officefloor.net>
2021-04-22 13:22:37 +0800

    Logging SSL protocol used

--- officefloor/server/officeserver/src/main/test/net/officefloor/server/ssl/OfficeFloorDefaultSslContextSource.java
+++ officefloor/server/officeserver/src/main/test/net/officefloor/server/ssl/OfficeFloorDefaultSslContextSource.java
@@ -147,6 +147,9 @@ public class OfficeFloorDefaultSslContextSource implements SslContextSource {
 
 							// Context created, so flag as the protocol used
 							protocolUsed = protocol;
+							
+							// Indicate protocol used for testing
+							System.err.println("Test server using protocol " + protocolUsed);
 
 							// Return the SSL context
 							return sslContext;
