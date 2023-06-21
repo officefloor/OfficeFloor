d2b764e68ba172cbdb967e626c3bbb3ed1236462
daniel <daniel@officefloor>
2023-06-21 23:38:23 +0800

    Initial Google Function project

---
+++ officefloor/server/officeserver_googlefunction/src/test/java/net/officefloor/server/google/function/OfficeFloorHttpFunctionIT.java
@@ -0,0 +1,22 @@
+package net.officefloor.server.google.function;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests the {@link OfficeFloorHttpFunction}.
+ * 
+ * @author Daniel Sagenschneider
+ */
+public class OfficeFloorHttpFunctionIT {
+
+	/**
+	 * Ensure can call {@link OfficeFloorHttpFunction}.
+	 */
+	@Test
+	public void request() {
+		fail("TODO implement");
+	}
+	
+}
