57246f1f0ddf0755a7e6b823a734784e2171f729
Daniel Sagenschneider <daniel@officefloor.net>
2023-07-25 12:02:17 +0800

    Initial work on Google Function maven plugin

---
+++ officefloor/maven/officefloor-googlefunction-maven-plugin/src/main/java/net/officefloor/maven/StartGoogleFunctionMojo.java
@@ -0,0 +1,24 @@
+package net.officefloor.maven;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.Mojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * {@link Mojo} to start Google Function.
+ */
+public class StartGoogleFunctionMojo extends AbstractMojo {
+
+	/*
+	 * ==================== AbstractMojo ====================
+	 */
+
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		
+		// Start
+		
+	}
+
+}
