729fb0060fc7a802924b30c421950858309835d3
Daniel Sagenschneider <daniel@officefloor.net>
2026-04-01 00:35:11 +0800

    Spring REST escalation handling tutorial

---
+++ tutorials/spring/SpringRestExceptionHttpServer/src/test/java/net/officefloor/spring/starter/rest/exception/MockException.java
@@ -0,0 +1,10 @@
+package net.officefloor.spring.starter.rest.exception;
+
+// START SNIPPET: tutorial
+public class MockException extends Exception {
+
+    public MockException(String message) {
+        super(message);
+    }
+}
+// END SNIPPET: tutorial
