bee16b095e02a499d06fa92c4cdfc2fcbcf62ace
Daniel Sagenschneider <daniel@officefloor.net>
2026-04-24 18:31:27 +0800

    Enable @CrossOrigin support

--- springboot/officefloor_rest_spring_boot_starter/src/main/java/net/officefloor/spring/starter/rest/OfficeFloorRestEndpoint.java
+++ springboot/officefloor_rest_spring_boot_starter/src/main/java/net/officefloor/spring/starter/rest/OfficeFloorRestEndpoint.java
@@ -1,14 +1,21 @@
 package net.officefloor.spring.starter.rest;
 
+import net.officefloor.compile.spi.office.ExecutionManagedFunction;
 import net.officefloor.compile.spi.officefloor.ExternalServiceInput;
 import net.officefloor.server.http.HttpMethod;
 import net.officefloor.spring.starter.rest.cors.ComposeCorsConfiguration;
 import net.officefloor.web.rest.build.RestEndpoint;
 import net.officefloor.web.rest.build.RestMethod;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.cors.CorsConfiguration;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * REST endpoint for handling by {@link net.officefloor.frame.api.manage.OfficeFloor}.
@@ -37,6 +44,63 @@ public class OfficeFloorRestEndpoint {
         return cors;
     }
 
+    /**
+     * Creates the {@link CorsConfiguration} from the {@link CrossOrigin} annotation.
+     *
+     * @param crossOrigin     {@link CrossOrigin} annotation.
+     * @param endpointMethods REST methods implemented.
+     * @return {@link CorsConfiguration}.
+     */
+    private static CorsConfiguration createCorsFromAnnotation(CrossOrigin crossOrigin, List<String> endpointMethods) {
+        CorsConfiguration cors = new CorsConfiguration();
+
+        String[] origins = crossOrigin.origins();
+        if (origins.length > 0) {
+            cors.setAllowedOrigins(Arrays.asList(origins));
+        }
+
+        String[] originPatterns = crossOrigin.originPatterns();
+        if (originPatterns.length > 0) {
+            cors.setAllowedOriginPatterns(Arrays.asList(originPatterns));
+        }
+
+        RequestMethod[] methods = crossOrigin.methods();
+        if (methods.length > 0) {
+            cors.setAllowedMethods(Arrays.stream(methods).map(RequestMethod::name).collect(Collectors.toList()));
+        } else {
+            cors.setAllowedMethods(endpointMethods);
+        }
+
+        String[] allowedHeaders = crossOrigin.allowedHeaders();
+        if (allowedHeaders.length > 0) {
+            cors.setAllowedHeaders(Arrays.asList(allowedHeaders));
+        }
+
+        String[] exposedHeaders = crossOrigin.exposedHeaders();
+        if (exposedHeaders.length > 0) {
+            cors.setExposedHeaders(Arrays.asList(exposedHeaders));
+        }
+
+        String allowCredentials = crossOrigin.allowCredentials();
+        if (!allowCredentials.isEmpty()) {
+            cors.setAllowCredentials(Boolean.parseBoolean(allowCredentials));
+        }
+
+        long maxAge = crossOrigin.maxAge();
+        if (maxAge >= 0) {
+            cors.setMaxAge(maxAge);
+        }
+
+        return cors;
+    }
+
+    /**
+     * Convenience method to combine {@link CorsConfiguration} allowing handling of <code>null</code>.
+     *
+     * @param corsA {@link CorsConfiguration} A.
+     * @param corsB {@link CorsConfiguration} B.
+     * @return Combined {@link CorsConfiguration} or <code>null</code> if both <code>null</code>.
+     */
     public static CorsConfiguration combineCors(CorsConfiguration corsA, CorsConfiguration corsB) {
         if (corsA != null) {
             if (corsB != null) {
@@ -56,7 +120,7 @@ public class OfficeFloorRestEndpoint {
     /**
      * {@link CorsConfiguration}.
      */
-    private final CorsConfiguration corsConfiguration;
+    private CorsConfiguration corsConfiguration;
 
     /**
      * {@link OfficeFloorRestMethod} instances.
@@ -95,9 +159,8 @@ public class OfficeFloorRestEndpoint {
             corsConfiguration = combineCors(corsConfiguration, methodCors);
 
             // Explore functions for further information
-            method.getServiceInput().addExecutionExplorer((executionContext) -> {
-                // TODO explore for @CrossOrigin and combine into resulting CorsConfiguration
-            });
+            method.getServiceInput().addExecutionExplorer((executionContext) ->
+                    exploreCrossOrigin(executionContext.getInitialManagedFunction(), new HashSet<>(), allowedMethods));
         }
 
         // Default allowed methods
@@ -107,6 +170,34 @@ public class OfficeFloorRestEndpoint {
 
         // Capture the CORS configuration
         this.corsConfiguration = corsConfiguration;
+    }
+
+    /**
+     * Recursively explores the {@link ExecutionManagedFunction} tree for {@link CrossOrigin} annotations.
+     *
+     * @param function {@link ExecutionManagedFunction}.
+     * @param visited Already visited {@link ExecutionManagedFunction}.
+     * @param endpointMethods REST methods supported.
+     */
+    private void exploreCrossOrigin(ExecutionManagedFunction function, Set<String> visited, List<String> endpointMethods) {
+
+        // Determine if visited or no function
+        if ((function == null) || (!visited.add(function.getManagedFunctionName()))) {
+            return;
+        }
+
+        // Combine in possible CORS information
+        CrossOrigin crossOrigin = function.getManagedFunctionType().getAnnotation(CrossOrigin.class);
+        if (crossOrigin != null) {
+            this.corsConfiguration = combineCors(this.corsConfiguration, createCorsFromAnnotation(crossOrigin, endpointMethods));
+            return;
+        }
+
+        // Explore further the tree
+        exploreCrossOrigin(function.getNextManagedFunction(), visited, endpointMethods);
+        for (var flowType : function.getManagedFunctionType().getFlowTypes()) {
+            exploreCrossOrigin(function.getManagedFunction(flowType), visited, endpointMethods);
+        }
     }
 
     /**
