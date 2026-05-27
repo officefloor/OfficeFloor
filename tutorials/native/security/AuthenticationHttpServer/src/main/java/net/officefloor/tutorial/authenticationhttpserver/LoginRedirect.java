55a180296f7fb9a90bdf967da1d1f481e2040f44
Daniel Sagenschneider <daniel@officefloor.net>
2026-05-28 00:40:08 +0800

    Tutorials now with REST YAML configuration

--- tutorials/native/security/AuthenticationHttpServer/src/main/java/net/officefloor/tutorial/authenticationhttpserver/LoginRedirect.java
+++ tutorials/native/security/AuthenticationHttpServer/src/main/java/net/officefloor/tutorial/authenticationhttpserver/LoginRedirect.java
@@ -2,14 +2,16 @@ package net.officefloor.tutorial.authenticationhttpserver;
 
 import java.io.IOException;
 
+import net.officefloor.server.http.HttpServerLocation;
 import net.officefloor.server.http.HttpStatus;
 import net.officefloor.server.http.ServerHttpConnection;
 
 public class LoginRedirect {
 
-	public void loginRedirect(ServerHttpConnection connection) throws IOException {
+	public void loginRedirect(ServerHttpConnection connection, HttpServerLocation serverLocation) throws IOException {
+		String url = serverLocation.createClientUrl(true, "/login");
 		connection.getResponse().setStatus(HttpStatus.SEE_OTHER);
-		connection.getResponse().getHeaders().addHeader("location", "/login");
+		connection.getResponse().getHeaders().addHeader("location", url);
 	}
 
 }
