651a58c6289894404b800d23098f17b5f0ee8413
Daniel Sagenschneider <daniel@officefloor.net>
2026-06-01 12:36:29 +0800

    Fixes to links for site release

--- site-linkcheck.sh
+++ site-linkcheck.sh
@@ -2,7 +2,7 @@
 
 # Ensure JAVA_HOME set (for JavaDoc)
 if [[ -z "$JAVA_HOME" ]]; then
-	>&2 echo "ERROR: JAVA_HOME not specified"  
+	>&2 echo "ERROR: JAVA_HOME not specified"
 	exit 1
 fi
 
@@ -18,6 +18,10 @@ rm -rf target/site
 mv target/staging/ target/site
 rm -rf target/linkcheck
 rm -rf target/site/linkcheck.html
+# Start Jetty to serve staged site (was stopped at end of site:stage post-site phase)
+mvn jetty:start -N
 # TODO: provide -N once NPE issue resolved
-mvn post-site
+mvn linkcheck:linkcheck
+# Stop Jetty
+mvn jetty:stop -N
 google-chrome target/site/linkcheck.html &
